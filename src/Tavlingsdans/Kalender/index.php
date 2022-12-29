2b90481143b03e1abd4f0329a8506dfb97a32eb9
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-12-29 19:10:47 +0100

    Lagt till footer.php och header.php på sidor

--- src/Tavlingsdans/Kalender/index.php
+++ src/Tavlingsdans/Kalender/index.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="sv">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tävlingskalender - Tävlingsdans - Rockrullarna.se</title>
-  <link href="/filer/bilder/Rockrullarna-favicon.png?w=32" rel="shortcut icon" type="image/x-icon">
-  <link rel="stylesheet" href="/filer/css/rockrullarna.css" />
-</head>
-<body>
-  <header>
-    <img alt="Logga för Dansklubben Rockrullarna" src="/filer/bilder/Rockrullarna-lar-dig-dansa-hos-oss.jpg" class="logo" />
-  </header>
-  <main>
+<?php
+  $header_title = "Kalender - Tävlingsdans";
+  include_once '../../includes/header.php'
+?>
     <div id="BreadCrumbsDiv">
-      <a href="../../">Hem</a> / <a href="../">Tävlingsdans</a> / <span>Tävlingskalender</span>
+      <a href="../../">Hem</a> / <a href="../">Tävlingsdans</a> / <span>Kalender</span>
     </div>
     <h1>
       Tävlingskalender
@@ -26,9 +15,6 @@
       Om hämtningen inte fungerar automatiskt, hittar ni tävlingskalendern via dans.se :<br />
       <a href="https://dans.se/tools/comp/events/?fed=dsf" title="Tävlingskalender på dans.se" target="_blank">https://dans.se/tools/comp/events/?fed=dsf</a>
     </p>
-  </main>
-  <footer>
-
-  </footer>
-</body>
-</html>
+<?php
+  include_once '../../includes/footer.php'
+?>
