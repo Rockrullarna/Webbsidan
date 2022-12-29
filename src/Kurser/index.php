1acd165034fec3f9423e7b14809be58518022360
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-12-29 19:53:53 +0100

    Lagt in en redirect i php-sidan /Kurser

--- src/Kurser/index.php
+++ src/Kurser/index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="sv">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Redirect to: Anmälan Danskurser - Rockrullarna</title>
-</head>
-<body>
-  <p>
-    REDIRECT /src/Danskurser/AnmalanDanskurser
-  </p>
-</body>
-</html>
+<?php
+  header('Location: ../Danskurser/Anmalan-danskurser');
+  exit;
+?>
