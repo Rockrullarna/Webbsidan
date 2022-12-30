b7e3f7c8fae12294121ea9a7ca76c2c8d5442d90
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-12-30 12:27:20 +0100

    Medlemsmöte - Skapat sidan

---
+++ src/Foreningen/Moten-och-protokoll/Medlemsmote/index.php
@@ -0,0 +1,67 @@
+<?php
+  $header_title = "Medlemsmöte - Möten och protokoll - Föreningen";
+  $header_description = "Här finner du våra publika medlemsmötes-protokoll från olika möten vi genomfört";
+
+  $page_updated = "2022-12-30 12:16";
+  $page_url = "/Foreningen/Moten-och-protokoll/Medlemsmote";
+  $page_contact_name = "Styrelsen";
+  $page_contact_email = "styrelsen@rockrullarna.se";
+
+  include_once '../../../includes/header.php'
+?>
+    <div id="BreadCrumbsDiv">
+      <a href="../../../">Rockrullarna.se</a> / <a href="../../">Föreningen</a> / <a href="../">Möten och protokoll</a> / <span>Medlemsmöte</span>
+    </div>
+    <h1>
+      Medlemsmöte
+    </h1>
+    <p>En gång per hösttermin anordnar föreningen ett medlemsmöte. På mötet presenterar styrelsen aktuella frågor och ärenden samt tar emot medlemmarnas synpunkter. Efter mötet avnjuter vi mat tillsammans och kvällen avslutas med dans. Lokal och upplägg för medlemsmötet kan variera från år till år.</p>
+    <p>Anmälan till årsmötet görs via sidan <a href="/Danskurser/Anmalan-danskurser" title="Anmälan danskurser" target="_top">Anmälan danskurser</a> under kategorin "Fest".</p>
+    <p>Information gällande medlemsmötet kommer ut via sociala medier, hemsida samt via tränare.</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <h2>Protokollen</h2>
+    <p>Här nedan samlas protokoll från medlemsmöten.</p>
+    <table class="GalleryTable" border="0" cellpadding="0" cellspacing="0">
+      <tr class="GalleryTableHeader">
+          <td colspan="2" class="paddingLeft5">Titel</td>
+          <td>Uppladdad</td>
+          <td>Filtyp</td>
+          <td>Storlek</td>
+      </tr>
+
+      <tr class='GalleryTableRowOdd'>
+          <td colspan="2" class="GalleryTableCellIcon">
+              <a href="/globalassets/dk-rockrullarna---danssport/dokument/protokoll-medlemsmoten/2019/2019-11-30--protokoll_medlemsmote_ht2019_dkrr.pdf?w=900&amp;h=900" title="2019-11-30--Protokoll_Medlemsmöte_HT2019_DKRR" target="_blank" rel="gallery">
+                  <img src="https://www7.idrottonline.se/images/design/Extensions/pdf.gif" width="18" height="18" />
+                  <span class="title">2019-11-30--Protokoll_Medlemsmöte_HT2019_DKRR</span>
+              </a>
+              <span class="description"></span>
+              <span class="creator"></span>   
+          </td>
+          <td class="GalleryTableCellFileDate">2020-01-23</td>
+          <td class="GalleryTableCellFileType">PDF</td>
+          <td class="GalleryTableCellFileSize">
+              2 MB
+          </td>
+      </tr>
+
+      <tr class='GalleryTableRowEven'>
+          <td colspan="2" class="GalleryTableCellIcon">
+              <a href="/globalassets/dk-rockrullarna---danssport/dokument/protokoll-medlemsmoten/2020/2020-12-10--protokoll-dkrr-medlemsmote-ht2020.pdf?w=900&amp;h=900" title="2020-12-10--Protokoll-DKRR-Medlemsmöte-HT2020" target="_blank" rel="gallery">
+                  <img src="https://www7.idrottonline.se/images/design/Extensions/pdf.gif" width="18" height="18" />
+                  <span class="title">2020-12-10--Protokoll-DKRR-Medlemsmöte-HT2020</span>
+              </a>
+              <span class="description"></span>
+              <span class="creator"></span>   
+          </td>
+          <td class="GalleryTableCellFileDate">2021-06-01</td>
+          <td class="GalleryTableCellFileType">PDF</td>
+          <td class="GalleryTableCellFileSize">
+              183 KB
+          </td>
+      </tr>
+    </table>
+<?php
+  include_once '../../../includes/footer.php'
+?>
