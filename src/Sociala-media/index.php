52b586c9b20596b1c784d0fc474fa6e353158cc4
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-12-30 01:38:10 +0100

    Uppdaterat sociala-media sidan

--- src/Sociala-media/index.php
+++ src/Sociala-media/index.php
@@ -2,7 +2,7 @@
   $header_title = "Sociala media flöden";
   $header_description = "Information om de sociala media-sidorna vi använder oss av för kommunikation";
 
-  $page_updated = "2022-12-29 23:44";
+  $page_updated = "2022-12-30 01:15";
   $page_url = "/Sociala-media";
   $page_contact_name = "Info";
   $page_contact_email = "info@rockrullarna.se";
@@ -21,14 +21,14 @@
       <!-- Feed uppdateras från: https://developers.facebook.com/docs/plugins/page-plugin/ -->
       <a href="https://fb.me/rockrullarna" title="Rockrullarna på facebook">fb.me/rockrullarna</a>
     </p>
+    <h2>Instagram</h2>
+    <p>
+      <a href="https://www.instagram.com/rockrullarna" title="Rockrullarna på instagram">instagram.com/rockrullarna</a>
+    </p>
     <h2>Tik-Tok</h2>
     <p><a href="https://www.tiktok.com/@dansklubbrockrullarna" title="Rockrullarna på tik tok">tiktok.com/@dansklubbrockrullarna</a></p>
     <p>
       <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@dansklubbrockrullarna" data-unique-id="dansklubbrockrullarna" data-embed-type="creator" style="max-width: 780px; min-width: 288px;" > <section> <a target="_blank" href="https://www.tiktok.com/@dansklubbrockrullarna?refer=creator_embed">@dansklubbrockrullarna</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>
-    </p>
-    <h2>Instagram</h2>
-    <p>
-      <a href="https://www.instagram.com/rockrullarna" title="Rockrullarna på instagram">instagram.com/rockrullarna</a>
     </p>
 <?php
   include_once '../includes/footer.php'
