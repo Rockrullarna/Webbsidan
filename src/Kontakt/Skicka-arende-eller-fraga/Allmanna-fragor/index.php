c7ffe4ebfa71e8e648f54b2c11a548a8d93fb549
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-12-31 18:44:19 +0100

    Skapat sidan allmänna frågor

---
+++ src/Kontakt/Skicka-arende-eller-fraga/Allmanna-fragor/index.php
@@ -0,0 +1,19 @@
+<?php
+  $header_title = "Allmänna frågor - Skicka ärende eller fråga - Kontakt";
+  $header_description = "Här kan du kontakta oss via e-post";
+
+  $page_updated = "2022-12-31 18:42";
+  $page_url = "/Kontakt/Skicka-arende-eller-fraga/Allmanna-fragor";
+  $page_contact_name = "Info";
+  $page_contact_email = "info@rockrullarna.se";
+
+  include_once '../../../includes/header.php'
+?>
+    <div id="BreadCrumbsDiv">
+      <a href="../../../">Rockrullarna.se</a> / <a href="../../">Kontakt</a> / <a href="../">Skicka ärende eller fråga</a> / <span>Allmäna frågor</span>
+    </div>
+    <h1>Allmäna frågor (info)</h1>
+    <p>När du kontaktar oss via webbformulär går ditt ärende direkt till områdesansvarig hos Rockrullarna. På så sätt kan vi ge dig bäst service med minst väntetid.</p>
+<?php
+  include_once '../../../includes/footer.php'
+?>
