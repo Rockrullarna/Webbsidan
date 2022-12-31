b0026234ccd3d94157f9b5c4ef064a219bf34a88
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-12-31 16:02:22 +0100

    Lagt till texten från gamla webbsidan

--- src/Kontakt/Fragor-och-svar/index.php
+++ src/Kontakt/Fragor-och-svar/index.php
@@ -18,6 +18,262 @@
     </p>
 
   
+    <div class="FAQCategoryHeading1">
+                Teams-möte
+            </div>
+
+    <div class="FAQquestion">
+        <p>Hur ansluter jag till Teams-m&ouml;tet som jag ska n&auml;rvara vid hos Rockrullarna?&nbsp;</p>
+    </div>
+
+    <span id="para1" class="faqFact DisplayNone">
+        <ol>
+    <li>Du b&ouml;rjar med att ansluta genom att &ouml;ppna den Teams-l&auml;nken du f&aring;tt av oss i inbjudan till din e-postadress (har du inte f&aring;tt n&aring;gon l&auml;nk, kontakta oss via:&nbsp;<a href="/Kontaktaoss/fragorochsvar/EmailEncoderEmbed.aspx?m=%7cpx%7b(t%7eHx%7du%7eN(r%7erz(r(u%7b%7bp(r%7dp8(st&amp;contentEncoding=utf-8&amp;l=sv" title="Skyddad adress"><img class="emailEncoderImage" alt="This is a mailto link" src="/IdrottOnlineKlubb/orebro/DKRockrullarna-Danssport/Kontaktaoss/fragorochsvar/EmailEncoderEmbed.aspx?it=(s(t(y(rt%7b(st%7dN(r%7erz(r(u%7b%7bp(r%7dp8(st" /></a>).&nbsp;</li>
+    <li>Se g&auml;rna till att ladda ned Teams p&aring; din dator, mobil eller surfplatta via den &ouml;versta knappen i l&auml;nken "Ladda ner app".</li>
+    <li>G&aring; sedan tillbaka till den inbjudan du f&aring;tt av oss och &ouml;ppna sedan Teams-m&ouml;tet igen.&nbsp;</li>
+    <li>V&auml;lj nu att starta m&ouml;tet via din Teams-app.&nbsp;</li>
+    <li>Se till att du har r&auml;tt inst&auml;llningar f&ouml;r h&ouml;gtalare/ljud p&aring; din enhet (kan &auml;ven &auml;ndras under m&ouml;tet om det beh&ouml;vs).&nbsp;</li>
+    <li>St&auml;ng g&auml;rna av mikrofonen p&aring; din enhet n&auml;r du anslutit till m&ouml;tet.&nbsp;</li>
+    <li>Nu ser vi till att ha ett riktigt trevligt m&ouml;te tillsammans!</li>
+    </ol>
+    <p>&nbsp;<span style="color: #ffffff;">.</span></p>
+    <p>Alla steg finns h&auml;r beskrivna i en kortare (2 min) informations-film:&nbsp;</p>
+    <p><iframe frameborder="0" allowfullscreen="allowfullscreen" height="315" src="https://www.youtube-nocookie.com/embed/_6rVoiQCPz4" title="YouTube video player" width="560"></iframe></p>
+    <p>Direktl&auml;nk till detta svaret:&nbsp;<br /><a href="https://www.Rockrullarna.se/KontaktaOss/FragorOchSvar/Teams-mote/AnslutaTillMote">https://www.Rockrullarna.se/KontaktaOss/FragorOchSvar/Teams-mote/AnslutaTillMote</a>&nbsp;</p>
+    </span>
+
+    <span id="linkShow1" class="DisplayBlock">
+        <div class="faq" onclick="return toggleMe(&#39;para1&#39;, &#39;linkShow1&#39;, &#39;linkHide1&#39;)">
+            <p>
+                Visa svar
+            </p>
+        </div>
+        <br />
+    </span>
+    <span id="linkHide1" class="DisplayNone">
+        <div class="faq" onclick="return toggleMe(&#39;para1&#39;, &#39;linkShow1&#39;, &#39;linkHide1&#39;)">
+            <p>
+                D&#246;lj svar
+            </p>
+        </div>
+        <br />
+    </span>
+
+                    
+                        
+                        
+
+    <div class="FAQquestion">
+        <p>Hur b&ouml;r jag agera under ett Teams-m&ouml;te?</p>
+    </div>
+
+    <span id="para2" class="faqFact DisplayNone">
+        <p>Under v&aring;ra Teams-m&ouml;ten s&aring; finns det n&aring;gra saker att t&auml;nka p&aring;, s&aring; vi inte pratar i mun p&aring; varandra och f&aring;r ett riktigt trevligt m&ouml;te tillsammans.</p>
+    <p><img src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/teams-mote/infoundermotet.png?w=720&amp;h=186" alt="" width="720" height="186" /></p>
+    <ul>
+    <li><strong>Beg&auml;r ordet</strong> n&auml;r du vill prata genom att trycka p&aring; "r&auml;ck upp handen figuren"🙋&zwj;♀️ (nr 1 i bilden) och sedan anv&auml;nda funktionen "R&auml;ck upp handen"✋ (nr 2 i bilden).&nbsp;<br /><br /></li>
+    <li><strong>Kom &aring;t chatten</strong> genom att trycka p&aring; "pratbubblan"💬 (nr 3 i bilden) och d&auml;r kan du skriva in dina fr&aring;gor n&auml;rsomhelst under m&ouml;tet. S&aring; besvaras dessa i turordning.&nbsp;<br /><br /></li>
+    <li><strong>St&auml;ng av ljudet</strong> genom att se till s&aring; "mikrofon-ikonen"🎤 (nr 4 i bilden) &auml;r &ouml;verstreckad.<br /><br /></li>
+    <li><strong>Skicka en reaktion</strong>&nbsp;kan du g&ouml;ra genom att trycka p&aring; "r&auml;ck upp handen figuren"🙋&zwj;♀️&nbsp;(nr 1 i bilden) och sedan n&auml;rsomhelst under m&ouml;tet skicka en "tumme upp"👍, "hj&auml;rta"💓, "appl&aring;d"👏 eller "skratt"😄 reaktion (nr 5 i bilden) under m&ouml;tets g&aring;ng, som kommer synas f&ouml;r alla deltagare.&nbsp;</li>
+    </ul>
+    <p><iframe frameborder="0" allowfullscreen="allowfullscreen" height="315" src="https://www.youtube-nocookie.com/embed/5DA5eYaQlR4" title="YouTube video player" width="560"></iframe></p>
+    <p>Direktl&auml;nk till detta svaret:&nbsp;<br /><a href="https://www.rockrullarna.se/KontaktaOss/FragorOchSvar/Teams-mote/InformationUnderMotet" title="Teams - Information under mötet">https://www.Rockrullarna.se/KontaktaOss/FragorOchSvar/Teams-mote/InformationUnderMotet</a>&nbsp;</p>
+    </span>
+
+    <span id="linkShow2" class="DisplayBlock">
+        <div class="faq" onclick="return toggleMe(&#39;para2&#39;, &#39;linkShow2&#39;, &#39;linkHide2&#39;)">
+            <p>
+                Visa svar
+            </p>
+        </div>
+        <br />
+    </span>
+    <span id="linkHide2" class="DisplayNone">
+        <div class="faq" onclick="return toggleMe(&#39;para2&#39;, &#39;linkShow2&#39;, &#39;linkHide2&#39;)">
+            <p>
+                D&#246;lj svar
+            </p>
+        </div>
+        <br />
+    </span>
+
+                    
+                
+            
+                <div class="FAQCategoryHeading1">
+                    Zoom-möte
+                </div>
+                
+                        
+                        
+
+    <div class="FAQquestion">
+        <p>Hur ansluter jag till Zoom-mötet via min Mobil/Surfplatta?</p>
+    </div>
+
+    <span id="para3" class="faqFact DisplayNone">
+        <p>1. Ladda ned Zoom via din applikations-butik (App Store/Google Play).</p>
+    <p>Länk till <a href="https://itunes.apple.com/us/app/id546505307" target="_blank" title="Zoom i App Store (iOS)" rel="noopener">Zoom i App Store (iOS)<br /></a>Länk till <a href="https://play.google.com/store/apps/details?id=us.zoom.videomeetings" target="_blank" title="Zoom i Google Play (Android)" rel="noopener">Zoom i Google Play (Android)</a></p>
+    <p><img title="Nedladdning av Zoom" src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/zoom-mote/zoom-mobil-1.jpg?w=135&amp;h=292" alt="Nedladdning av Zoom" width="135" height="292" /></p>
+    <p>2. När du har installerat Zoom på din mobil eller surfplatta, så kan du navigera till (trycka på) länken du fått i e-postmeddelandet med inbjudan till mötet. </p>
+    <p>3. Nu får du skriva in ditt Förnamn, Efternamn och E-postadress för att ansluta till mötet (skriv gärna ditt fulla namn så vi kan pricka av din närvaro): </p>
+    <p><img title="Skriv in namn och epost" src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/zoom-mote/zoom-mobil-2.png?w=282&amp;h=609" alt="Skriv in namn och epost" width="282" height="609" /></p>
+    <p>4. Antingen så kommer du nu in i mötet, eller får ett meddelande om att värden behöver starta mötet ("Waiting for the host to start this webinar", eller ansluter). </p>
+    <p><img title="Väntar på att ansluta" src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/zoom-mote/zoom-mobil-3.png?w=208&amp;h=449" alt="Väntar på att ansluta" width="208" height="449" />       <img title="Ansluten till mötet" src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/zoom-mote/zoom-mobil-4.png?w=207&amp;h=447" alt="Ansluten till mötet" width="207" height="447" /></p>
+    <p>5. Under mötets gång kan det hända att knapparna försvinner, trycker du bara till en gång på skärmen så kommer knapparna tillbaka för att "Räcka upp handen" , Chatta eller skriva frågor till oss. </p>
+    <p><img title="Knapparna är borta" src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/zoom-mote/zoom-mobil-5.png?w=282&amp;h=609" alt="Knapparna är borta" width="282" height="609" /></p>
+    <p>Trycker du bara till på skärmen, så kommer alla knappar fram igen: </p>
+    <p><img title="Knapparna i mötet visas efter ett tryck på bildskärmen" src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/zoom-mote/zoom-mobil-6.png?w=282&amp;h=609" alt="Knapparna i mötet visas efter ett tryck på bildskärmen" width="282" height="609" /></p>
+    <p> </p>
+    <p>Vi hoppas nu att allt ska fungera bra under mötet vi kommer att ha här via Zoom. </p>
+    <p>Vi ses online! :) </p>
+    </span>
+
+    <span id="linkShow3" class="DisplayBlock">
+        <div class="faq" onclick="return toggleMe(&#39;para3&#39;, &#39;linkShow3&#39;, &#39;linkHide3&#39;)">
+            <p>
+                Visa svar
+            </p>
+        </div>
+        <br />
+    </span>
+    <span id="linkHide3" class="DisplayNone">
+        <div class="faq" onclick="return toggleMe(&#39;para3&#39;, &#39;linkShow3&#39;, &#39;linkHide3&#39;)">
+            <p>
+                D&#246;lj svar
+            </p>
+        </div>
+        <br />
+    </span>
+
+                    
+                        
+                        
+
+    <div class="FAQquestion">
+        <p>Hur ansluter jag till Zoom-mötet via min Dator?</p>
+    </div>
+
+    <span id="para4" class="faqFact DisplayNone">
+        <p>1. Navigera till länken du fått i e-postmeddelandet med inbjudan till mötet. </p>
+    <p>2. Ladda ned och installera Zoom ifrån länken till inbjudan (Klicka på: download &amp; run Zoom, sedan installera det programmet som laddades ned i nedre delen av webbläsaren). </p>
+    <p><img title="Ladda ned och installera Zoom" src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/zoom-mote/zoom-dator-1-klicka-pa-nedladdade-filen.png?w=498&amp;h=424" alt="Ladda ned och installera Zoom" width="498" height="424" /></p>
+    <p>3. Efter installationen av Zoom är klar, klicka igen på länken ifrån mailet med inbjudan, och sedan ska det gå att starta Zoom via "öppna". </p>
+    <p><img title="Öppna zoom" src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/zoom-mote/zoom-dator-2-ladda-ned.png?w=497&amp;h=394" alt="Öppna zoom" width="497" height="394" /> </p>
+    <p>4. Nu bör du vara inne i Zoom-mötet. </p>
+    <p> </p>
+    <p>Vi hoppas nu att allt ska fungera bra under mötet vi kommer att ha här via Zoom. </p>
+    <p>Vi ses online! :) </p>
+    </span>
+
+    <span id="linkShow4" class="DisplayBlock">
+        <div class="faq" onclick="return toggleMe(&#39;para4&#39;, &#39;linkShow4&#39;, &#39;linkHide4&#39;)">
+            <p>
+                Visa svar
+            </p>
+        </div>
+        <br />
+    </span>
+    <span id="linkHide4" class="DisplayNone">
+        <div class="faq" onclick="return toggleMe(&#39;para4&#39;, &#39;linkShow4&#39;, &#39;linkHide4&#39;)">
+            <p>
+                D&#246;lj svar
+            </p>
+        </div>
+        <br />
+    </span>
+
+                    
+                        
+                        
+
+    <div class="FAQquestion">
+        <p>Hur ansluter jag till Zoom-mötet via min webbläsare?</p>
+    </div>
+
+    <span id="para5" class="faqFact DisplayNone">
+        <p>1. Navigera till länken du fått i e-postmeddelandet med inbjudan till mötet. </p>
+    <p>2. Du bör nu få valet att antingen ladda ned Zoom till din dator, eller ansluta via webbläsaren, välj då "Start from your browser" (starta från din webbläsare) , även om det är rekommenterat att du istället laddar ned Zoom och sedan, följer guiden: "Hur ansluter jag till mötet via datorn", eftersom du då får tillgång till fler funktioner via dator-versionen än webb-versionen av Zoom. <br /><img title="Starta från webbläsaren" src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/zoom-mote/zoom-1-starta-fran-webblasaren.png?w=534&amp;h=427" alt="Starta från webbläsaren" width="534" height="427" /></p>
+    <p>3. Tryck på "Join Audio by Computer"<br /><img title="Tryck på &quot;Join Audio by Computer&quot;" src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/zoom-mote/zoom-2-join-audio.png?w=534&amp;h=584" alt="Tryck på &quot;Join Audio by Computer&quot;" width="534" height="584" /></p>
+    <p>4. Tillåt användning av mikrofon/webbkamera i din webbläsare om du vill kunna höras/synas under mötet:<br /><img title="Tillåt användning av mikrofon/webbkamera" src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/zoom-mote/zoom-3-tillat.png?w=534&amp;h=498" alt="Tillåt användning av mikrofon/webbkamera" width="534" height="498" /></p>
+    <p>5. Nu är du inne i mötet, längst ned i mötet ser du knappar för att starta Mikrofon/Webbkamera eller Chatten. <br /><img title="Nu är du inne i mötet" src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/zoom-mote/zoom-4-mic.png?w=534&amp;h=426" alt="Nu är du inne i mötet" width="534" height="426" /></p>
+    <p>6. För att kunna vara med på omröstningar behöver du tillåta Pop-up fönster, som dyker upp när en omröstning startar (detta fungerar direkt om du har installerat Zoom). <br /><br /><br />Vi hoppas nu att allt ska fungera bra under mötet vi kommer att ha här via Zoom. </p>
+    <p>Vi ses online! :) </p>
+    </span>
+
+    <span id="linkShow5" class="DisplayBlock">
+        <div class="faq" onclick="return toggleMe(&#39;para5&#39;, &#39;linkShow5&#39;, &#39;linkHide5&#39;)">
+            <p>
+                Visa svar
+            </p>
+        </div>
+        <br />
+    </span>
+    <span id="linkHide5" class="DisplayNone">
+        <div class="faq" onclick="return toggleMe(&#39;para5&#39;, &#39;linkShow5&#39;, &#39;linkHide5&#39;)">
+            <p>
+                D&#246;lj svar
+            </p>
+        </div>
+        <br />
+    </span>
+
+                    
+                
+            
+                <div class="FAQCategoryHeading1">
+                    Dans.se
+                </div>
+                
+                        
+                        
+
+    <div class="FAQquestion">
+        <p>Hur loggar jag in och &auml;ndrar jag mina personuppgifter i dans.se?</p>
+    </div>
+
+    <span id="para6" class="faqFact DisplayNone">
+        <ol>
+    <li>G&aring; in via <a href="https://dans.se/" title="dans.se" target="_blank" rel="noopener">dans.se</a></li>
+    <li>Om du inte kommer ih&aring;g dina inloggningsuppgifter g&aring; via "Har redan ett konto men gl&ouml;mt dina uppgifter?"&nbsp;</li>
+    <li>Fyll i ditt f&ouml;rnamn och din e-postadress, samt tryck p&aring;: "Skicka inloggningsnyckel"</li>
+    <li>Du f&aring;r nu ett mail till din mailadress s&aring; du kan s&auml;tta ett l&ouml;senord till ditt konto.&nbsp;</li>
+    <li>Logga sedan in i <a href="https://dans.se/" title="dans.se" target="_blank" rel="noopener">dans.se</a>&nbsp;-&gt;&nbsp;"Logga in med befintliga inloggningsuppgifter"</li>
+    <li>N&auml;r du &auml;r inloggad p&aring; ditt dans.se-konto, s&aring; trycker du p&aring;: "<a class="linkSmallIconLeft smallIconEdit" href="https://dans.se/myaccount/edit/contact_info/">Mina personuppgifter</a>"&nbsp;</li>
+    <li>H&auml;r kan du nu &auml;ndra dina personuppgifter som alla dansklubbar anv&auml;nder om dig vid anm&auml;lningar till danskurser.&nbsp;</li>
+    </ol>
+    <p>&nbsp;Bilder finns nedan som hj&auml;lper dig i n&aring;gra steg h&auml;r ovan.&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>Lyckas du inte sj&auml;lv &auml;ndra dina personuppgifter i dans.se, s&aring; kan du alltid kontakta oss via: <a href="/Kontaktaoss/fragorochsvar/EmailEncoderEmbed.aspx?m=%7cpx%7b(t%7eHz(u(r(st(rN(r%7erz(r(u%7b%7bp(r%7dp8(st&amp;contentEncoding=utf-8&amp;l=sv" title="Skyddad adress"><img class="emailEncoderImage" alt="This is a mailto link" src="/IdrottOnlineKlubb/orebro/DKRockrullarna-Danssport/Kontaktaoss/fragorochsvar/EmailEncoderEmbed.aspx?it=z(u(r(st(rN(r%7erz(r(u%7b%7bp(r%7dp8(st" /></a>&nbsp;eller n&aring;gon i receptionen, s&aring; hj&auml;lper vi dig f&aring; korrekta uppgifter i systemet.</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p><strong>&nbsp;Bild till steg 2:&nbsp;</strong><br /><br /><img src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/dans.se/dans-se-inloggning1.png?w=600&amp;h=417" alt="" width="600" height="417" /></p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p><strong>Bild till steg 3:</strong><br /><br /><img src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/dans.se/dans-se-inloggning2.png?w=600&amp;h=397" alt="" width="600" height="397" /></p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p><strong>Bild till steg 6:</strong><br /><br /><img src="https://www7.idrottonline.se/globalassets/dk-rockrullarna---danssport/bilder/fragor_och_svar/dans.se/dans-se-inloggning3.png?w=600&amp;h=447" alt="" width="600" height="447" /></p>
+    <p>&nbsp;</p>
+    <p>Direktl&auml;nk till detta svaret:&nbsp;<br /><a href="https://www.rockrullarna.se/KontaktaOss/FragorOchSvar/Dans.se/AndraMinaPersonuppgifter">https://www.Rockrullarna.se/KontaktaOss/FragorOchSvar/Dans.se/AndraMinaPersonuppgifter</a>&nbsp;</p>
+    </span>
+
+    <span id="linkShow6" class="DisplayBlock">
+        <div class="faq" onclick="return toggleMe(&#39;para6&#39;, &#39;linkShow6&#39;, &#39;linkHide6&#39;)">
+            <p>
+                Visa svar
+            </p>
+        </div>
+        <br />
+    </span>
+    <span id="linkHide6" class="DisplayNone">
+        <div class="faq" onclick="return toggleMe(&#39;para6&#39;, &#39;linkShow6&#39;, &#39;linkHide6&#39;)">
+            <p>
+                D&#246;lj svar
+            </p>
+        </div>
+        <br />
+    </span>
 <?php
   include_once '../../includes/footer.php'
 ?>
