2c71eb81305bb311c58dd8643b0b06691e48bdf3
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-12-31 17:02:22 +0100

    Uppdaterat bild-länkar och IDn på sidan

--- src/Kontakt/Fragor-och-svar/index.php
+++ src/Kontakt/Fragor-och-svar/index.php
@@ -2,7 +2,7 @@
   $header_title = "Frågor och svar (FAQ)";
   $header_description = "Kontaktinformation till den ideella föreningen Dansklubben Rockrullarna i Örebro";
 
-  $page_updated = "2022-12-31 15:54";
+  $page_updated = "2022-12-31 16:48";
   $page_url = "/Kontakt/Fragor-och-svar";
   $page_contact_name = "Info";
   $page_contact_email = "info@rockrullarna.se";
@@ -17,10 +17,10 @@
       Här finner du svar på vanliga frågor som ställs till oss (engelska: Frequently Asked Questions - FAQ).
     </p>
 
-    <h2 id="Teams-mote" class="FAQCategoryHeading1">
+    <h2 id="teams-mote" class="FAQCategoryHeading1">
       Teams-möte
     </h2>
-    <h3 class="FAQquestion">
+    <h3 id="teams-ansluta-till-mote" class="FAQquestion">
         <p>Hur ansluter jag till Teams-mötet som jag ska närvara vid hos Rockrullarna? </p>
     </h3>
 
@@ -36,7 +36,7 @@
     </ol>
     <p class="mt-5">Alla steg finns här beskrivna i en kortare (2 min) informations-film: </p>
     <p><iframe frameborder="0" allowfullscreen="allowfullscreen" height="315" src="https://www.youtube-nocookie.com/embed/_6rVoiQCPz4" title="YouTube video player" width="560"></iframe></p>
-    <p>Direktlänk till detta svaret: <br /><a href="https://www.Rockrullarna.se/KontaktaOss/FragorOchSvar/Teams-mote/AnslutaTillMote">https://www.Rockrullarna.se/KontaktaOss/FragorOchSvar/Teams-mote/AnslutaTillMote</a> </p>
+    <p>Direktlänk till detta svaret: <br /><a href="#teams-ansluta-till-mote">https://www.Rockrullarna.se/Kontakt/Fragor-och-svar#teams-ansluta-till-mote</a></p>
     </span>
 
     <span id="linkShow1" class="DisplayBlock">
@@ -62,7 +62,7 @@
 
     <span id="para2" class="faqFact DisplayNone">
         <p>Under våra Teams-möten så finns det några saker att tänka på, så vi inte pratar i mun på varandra och får ett riktigt trevligt möte tillsammans.</p>
-    <p><img  src="./bilder/teams-mote/infoundermotet.png" alt="" width="720" height="186" /></p>
+    <p><img class="no-visual-search"src="./Teams-mote/bild-Teams-InfoUnderMotet.png" alt="Teams information under mötet" width="720" height="186" /></p>
     <ul>
     <li><strong>Begär ordet</strong> när du vill prata genom att trycka på "räck upp handen figuren"🙋&zwj;♀️ (nr 1 i bilden) och sedan använda funktionen "Räck upp handen"✋ (nr 2 i bilden). <br /><br /></li>
     <li><strong>Kom åt chatten</strong> genom att trycka på "pratbubblan"💬 (nr 3 i bilden) och där kan du skriva in dina frågor närsomhelst under mötet. Så besvaras dessa i turordning. <br /><br /></li>
@@ -81,7 +81,7 @@
         </div>
         <br />
     </span>
-    <span id="linkHide2" class="DisplayNone"> src="./bilder/
+    <span id="linkHide2" class="DisplayNone">
         <div class="faq" onclick="return toggleMe(&#39;para2&#39;, &#39;linkShow2&#39;, &#39;linkHide2&#39;)">
             <p>
                 D&#246;lj svar
@@ -90,7 +90,7 @@
         <br />
     </span>
 
-    <h2 id="Zoom-mote" class="FAQCategoryHeading1">
+    <h2 id="zoom-mote" class="FAQCategoryHeading1">
         Zoom-möte
     </h2>
     <h3 class="FAQquestion">
@@ -100,16 +100,16 @@
     <span id="para3" class="faqFact DisplayNone">
         <p>1. Ladda ned Zoom via din applikations-butik (App Store/Google Play).</p>
     <p>Länk till <a href="https://itunes.apple.com/us/app/id546505307" target="_blank" title="Zoom i App Store (iOS)" rel="noopener">Zoom i App Store (iOS)<br /></a>Länk till <a href="https://play.google.com/store/apps/details?id=us.zoom.videomeetings" target="_blank" title="Zoom i Google Play (Android)" rel="noopener">Zoom i Google Play (Android)</a></p>
-    <p><img title="Nedladdning av Zoom"  src="./bilder/zoom-mote/zoom-mobil-1.jpg" alt="Nedladdning av Zoom" width="135" height="292" /></p>
+    <p><img class="no-visual-search"title="Nedladdning av Zoom" src="./Zoom-mote/bild-Zoom-mobil-1.jpg" alt="Nedladdning av Zoom" width="270" height="584" /></p>
     <p>2. När du har installerat Zoom på din mobil eller surfplatta, så kan du navigera till (trycka på) länken du fått i e-postmeddelandet med inbjudan till mötet. </p>
     <p>3. Nu får du skriva in ditt Förnamn, Efternamn och E-postadress för att ansluta till mötet (skriv gärna ditt fulla namn så vi kan pricka av din närvaro): </p>
-    <p><img title="Skriv in namn och epost"  src="./bilder/zoom-mote/zoom-mobil-2.png" alt="Skriv in namn och epost" width="282" height="609" /></p>
+    <p><img class="no-visual-search"title="Skriv in namn och epost" src="./Zoom-mote/bild-Zoom-mobil-2.png" alt="Skriv in namn och epost" width="282" height="609" /></p>
     <p>4. Antingen så kommer du nu in i mötet, eller får ett meddelande om att värden behöver starta mötet ("Waiting for the host to start this webinar", eller ansluter). </p>
-    <p><img title="Väntar på att ansluta"  src="./bilder/zoom-mote/zoom-mobil-3.png" alt="Väntar på att ansluta" width="208" height="449" />       <img title="Ansluten till mötet"  src="./bilder/zoom-mote/zoom-mobil-4.png" alt="Ansluten till mötet" width="207" height="447" /></p>
+    <p><img class="no-visual-search"title="Väntar på att ansluta" src="./Zoom-mote/bild-Zoom-mobil-3.png" alt="Väntar på att ansluta" width="208" height="449" />       <img class="no-visual-search"title="Ansluten till mötet" src="./Zoom-mote/bild-Zoom-mobil-4.png" alt="Ansluten till mötet" width="207" height="447" /></p>
     <p>5. Under mötets gång kan det hända att knapparna försvinner, trycker du bara till en gång på skärmen så kommer knapparna tillbaka för att "Räcka upp handen" , Chatta eller skriva frågor till oss. </p>
-    <p><img title="Knapparna är borta"  src="./bilder/zoom-mote/zoom-mobil-5.png" alt="Knapparna är borta" width="282" height="609" /></p>
+    <p><img class="no-visual-search"title="Knapparna är borta" src="./Zoom-mote/bild-Zoom-mobil-5.png" alt="Knapparna är borta" width="282" height="609" /></p>
     <p>Trycker du bara till på skärmen, så kommer alla knappar fram igen: </p>
-    <p><img title="Knapparna i mötet visas efter ett tryck på bildskärmen"  src="./bilder/zoom-mote/zoom-mobil-6.png" alt="Knapparna i mötet visas efter ett tryck på bildskärmen" width="282" height="609" /></p>
+    <p><img class="no-visual-search"title="Knapparna i mötet visas efter ett tryck på bildskärmen" src="./Zoom-mote/bild-Zoom-mobil-6.png" alt="Knapparna i mötet visas efter ett tryck på bildskärmen" width="282" height="609" /></p>
     <p> </p>
     <p>Vi hoppas nu att allt ska fungera bra under mötet vi kommer att ha här via Zoom. </p>
     <p>Vi ses online! :) </p>
@@ -139,9 +139,9 @@
     <span id="para4" class="faqFact DisplayNone">
         <p>1. Navigera till länken du fått i e-postmeddelandet med inbjudan till mötet. </p>
     <p>2. Ladda ned och installera Zoom ifrån länken till inbjudan (Klicka på: download &amp; run Zoom, sedan installera det programmet som laddades ned i nedre delen av webbläsaren). </p>
-    <p><img title="Ladda ned och installera Zoom"  src="./bilder/zoom-mote/zoom-dator-1-klicka-pa-nedladdade-filen.png" alt="Ladda ned och installera Zoom" width="498" height="424" /></p>
+    <p><img class="no-visual-search"title="Ladda ned och installera Zoom" src="./Zoom-mote/bild-Zoom-dator-1-klicka-pa-nedladdade-filen.png" alt="Ladda ned och installera Zoom" width="498" height="424" /></p>
     <p>3. Efter installationen av Zoom är klar, klicka igen på länken ifrån mailet med inbjudan, och sedan ska det gå att starta Zoom via "öppna". </p>
-    <p><img title="Öppna zoom"  src="./bilder/zoom-mote/zoom-dator-2-ladda-ned.png" alt="Öppna zoom" width="497" height="394" /> </p>
+    <p><img class="no-visual-search"title="Öppna zoom" src="./Zoom-mote/bild-Zoom-dator-2-ladda-ned.png" alt="Öppna zoom" width="497" height="394" /> </p>
     <p>4. Nu bör du vara inne i Zoom-mötet. </p>
     <p> </p>
     <p>Vi hoppas nu att allt ska fungera bra under mötet vi kommer att ha här via Zoom. </p>
@@ -171,10 +171,10 @@
 
     <span id="para5" class="faqFact DisplayNone">
         <p>1. Navigera till länken du fått i e-postmeddelandet med inbjudan till mötet. </p>
-    <p>2. Du bör nu få valet att antingen ladda ned Zoom till din dator, eller ansluta via webbläsaren, välj då "Start from your browser" (starta från din webbläsare) , även om det är rekommenterat att du istället laddar ned Zoom och sedan, följer guiden: "Hur ansluter jag till mötet via datorn", eftersom du då får tillgång till fler funktioner via dator-versionen än webb-versionen av Zoom. <br /><img title="Starta från webbläsaren"  src="./bilder/zoom-mote/zoom-1-starta-fran-webblasaren.png" alt="Starta från webbläsaren" width="534" height="427" /></p>
-    <p>3. Tryck på "Join Audio by Computer"<br /><img title="Tryck på &quot;Join Audio by Computer&quot;"  src="./bilder/zoom-mote/zoom-2-join-audio.png" alt="Tryck på &quot;Join Audio by Computer&quot;" width="534" height="584" /></p>
-    <p>4. Tillåt användning av mikrofon/webbkamera i din webbläsare om du vill kunna höras/synas under mötet:<br /><img title="Tillåt användning av mikrofon/webbkamera"  src="./bilder/zoom-mote/zoom-3-tillat.png" alt="Tillåt användning av mikrofon/webbkamera" width="534" height="498" /></p>
-    <p>5. Nu är du inne i mötet, längst ned i mötet ser du knappar för att starta Mikrofon/Webbkamera eller Chatten. <br /><img title="Nu är du inne i mötet"  src="./bilder/zoom-mote/zoom-4-mic.png" alt="Nu är du inne i mötet" width="534" height="426" /></p>
+    <p>2. Du bör nu få valet att antingen ladda ned Zoom till din dator, eller ansluta via webbläsaren, välj då "Start from your browser" (starta från din webbläsare) , även om det är rekommenterat att du istället laddar ned Zoom och sedan, följer guiden: "Hur ansluter jag till mötet via datorn", eftersom du då får tillgång till fler funktioner via dator-versionen än webb-versionen av Zoom. <br /><img class="no-visual-search"title="Starta från webbläsaren" src="./Zoom-mote/bild-Zoom-1-starta-fran-webblasaren.png" alt="Starta från webbläsaren" width="534" height="427" /></p>
+    <p>3. Tryck på "Join Audio by Computer"<br /><img class="no-visual-search"title="Tryck på &quot;Join Audio by Computer&quot;" src="./Zoom-mote/bild-Zoom-2-Join-audio.png" alt="Tryck på &quot;Join Audio by Computer&quot;" width="534" height="584" /></p>
+    <p>4. Tillåt användning av mikrofon/webbkamera i din webbläsare om du vill kunna höras/synas under mötet:<br /><img class="no-visual-search"title="Tillåt användning av mikrofon/webbkamera" src="./Zoom-mote/bild-Zoom-3-Tillat.png" alt="Tillåt användning av mikrofon/webbkamera" width="534" height="498" /></p>
+    <p>5. Nu är du inne i mötet, längst ned i mötet ser du knappar för att starta Mikrofon/Webbkamera eller Chatten. <br /><img class="no-visual-search"title="Nu är du inne i mötet" src="./Zoom-mote/bild-Zoom-4-Mic.png" alt="Nu är du inne i mötet" width="534" height="426" /></p>
     <p>6. För att kunna vara med på omröstningar behöver du tillåta Pop-up fönster, som dyker upp när en omröstning startar (detta fungerar direkt om du har installerat Zoom). <br /><br /><br />Vi hoppas nu att allt ska fungera bra under mötet vi kommer att ha här via Zoom. </p>
     <p>Vi ses online! :) </p>
     </span>
@@ -214,17 +214,17 @@
     <li>Här kan du nu ändra dina personuppgifter som alla dansklubbar använder om dig vid anmälningar till danskurser. </li>
     </ol>
     <p> Bilder finns nedan som hjälper dig i några steg här ovan. </p>
-    <p> </p>
-    <p>Lyckas du inte själv ändra dina personuppgifter i dans.se, så kan du alltid kontakta oss via: <a href="/Kontaktaoss/fragorochsvar/EmailEncoderEmbed.aspx?m=%7cpx%7b(t%7eHz(u(r(st(rN(r%7erz(r(u%7b%7bp(r%7dp8(st&amp;contentEncoding=utf-8&amp;l=sv" title="Skyddad adress"><img class="emailEncoderImage" alt="This is a mailto link" src="/IdrottOnlineKlubb/orebro/DKRockrullarna-Danssport/Kontaktaoss/fragorochsvar/EmailEncoderEmbed.aspx?it=z(u(r(st(rN(r%7erz(r(u%7b%7bp(r%7dp8(st" /></a> eller någon i receptionen, så hjälper vi dig få korrekta uppgifter i systemet.</p>
-    <p> </p>
-    <p> </p>
-    <p><strong> Bild till steg 2: </strong><br /><br /><img  src="./bilder/dans.se/dans-se-inloggning1.png" alt="" width="600" height="417" /></p>
-    <p> </p>
-    <p> </p>
-    <p><strong>Bild till steg 3:</strong><br /><br /><img  src="./bilder/dans.se/dans-se-inloggning2.png" alt="" width="600" height="397" /></p>
-    <p> </p>
-    <p> </p>
-    <p><strong>Bild till steg 6:</strong><br /><br /><img  src="./bilder/dans.se/dans-se-inloggning3.png" alt="" width="600" height="447" /></p>
+    <p><br /></p>
+    <p>Lyckas du inte själv ändra dina personuppgifter i dans.se, så kan du alltid kontakta oss via: <a href="/Kontaktaoss/fragorochsvar/EmailEncoderEmbed.aspx?m=%7cpx%7b(t%7eHz(u(r(st(rN(r%7erz(r(u%7b%7bp(r%7dp8(st&amp;contentEncoding=utf-8&amp;l=sv" title="Skyddad adress"><img class="no-visual-search"class="emailEncoderImage" alt="This is a mailto link" src="/IdrottOnlineKlubb/orebro/DKRockrullarna-Danssport/Kontaktaoss/fragorochsvar/EmailEncoderEmbed.aspx?it=z(u(r(st(rN(r%7erz(r(u%7b%7bp(r%7dp8(st" /></a> eller någon i receptionen, så hjälper vi dig få korrekta uppgifter i systemet.</p>
+    <p><br /><br /></p>
+    <p><strong> Bild till steg 2 - Glömt dina uppgifter?: </strong></p>
+    <p><img class="no-visual-search"src="./Dans.se/bild-Dans-se-inloggning1.png" alt="Dans.se - Glömt dina uppgifter?" width="600" height="417" /></p>
+    <p><br /><br /></p>
+    <p><strong>Bild till steg 3 - Fyll i förnamn och e-post:</strong></p>
+    <p><img class="no-visual-search"src="./Dans.se/bild-Dans-se-inloggning2.png" alt="Fyll i förnamn och e-post" width="600" height="397" /></p>
+    <p><br /><br /></p>
+    <p><strong>Bild till steg 6 - Mina personuppgifter:</strong></p>
+    <p><img class="no-visual-search"src="./Dans.se/bild-Dans-se-inloggning3.png" alt="Mina personuppgifter" width="600" height="447" /></p>
     <p> </p>
     <p>Direktlänk till detta svaret: <br /><a href="https://www.rockrullarna.se/KontaktaOss/FragorOchSvar/Dans.se/AndraMinaPersonuppgifter">https://www.Rockrullarna.se/KontaktaOss/FragorOchSvar/Dans.se/AndraMinaPersonuppgifter</a> </p>
     </span>
