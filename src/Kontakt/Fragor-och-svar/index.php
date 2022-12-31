60aa1b35733d96e84a8c517768fd3a4cb92e4c69
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-12-31 16:01:37 +0100

    Lagt till sidan "Frågor och Svar"

---
+++ src/Kontakt/Fragor-och-svar/index.php
@@ -0,0 +1,23 @@
+<?php
+  $header_title = "Frågor och svar (FAQ)";
+  $header_description = "Kontaktinformation till den ideella föreningen Dansklubben Rockrullarna i Örebro";
+
+  $page_updated = "2022-12-31 15:54";
+  $page_url = "/Kontakt/Fragor-och-svar";
+  $page_contact_name = "Info";
+  $page_contact_email = "info@rockrullarna.se";
+
+  include_once '../../includes/header.php'
+?>
+    <div id="BreadCrumbsDiv">
+      <a href="../../">Rockrullarna.se</a> / <a href="../">Kontakt</a> / <span>Frågor och svar</span>
+    </div>
+    <h1>Vanliga frågor och svar (FAQ)</h1>
+    <p>
+      Här finner du svar på vanliga frågor som ställs till oss (engelska: Frequently Asked Questions - FAQ).
+    </p>
+
+  
+<?php
+  include_once '../../includes/footer.php'
+?>
