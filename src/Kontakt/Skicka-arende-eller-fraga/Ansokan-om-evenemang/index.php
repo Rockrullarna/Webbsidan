5388dd474a58ff15f7d266bcaea57fdaf980f88c
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-12-31 18:54:58 +0100

    Lagt till sidan /Ansokan-om-evenemang

---
+++ src/Kontakt/Skicka-arende-eller-fraga/Ansokan-om-evenemang/index.php
@@ -0,0 +1,19 @@
+<?php
+  $header_title = "Ansökan om evenemang - Skicka ärende eller fråga - Kontakt";
+  $header_description = "Här kan du kontakta oss via e-post";
+
+  $page_updated = "2022-12-31 18:53";
+  $page_url = "/Kontakt/Skicka-arende-eller-fraga/Ansokan-om-evenemang";
+  $page_contact_name = "Styrelsen";
+  $page_contact_email = "styrelsen@rockrullarna.se";
+
+  include_once '../../../includes/header.php'
+?>
+    <div id="BreadCrumbsDiv">
+      <a href="../../../">Rockrullarna.se</a> / <a href="../../">Kontakt</a> / <a href="../">Skicka ärende eller fråga</a> / <span>Ansökan om evenemang</span>
+    </div>
+    <h1>Ansökan om evenemang (styrelsen)</h1>
+    <p>När du kontaktar oss via webbformulär går ditt ärende direkt till områdesansvarig hos Rockrullarna. På så sätt kan vi ge dig bäst service med minst väntetid.</p>
+<?php
+  include_once '../../../includes/footer.php'
+?>
