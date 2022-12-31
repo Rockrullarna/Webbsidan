bdc8d1fca9487c6d0ecb7f20a54a7400749330ac
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-12-31 19:47:06 +0100

    Skapat Hur-loggar-jag-in sidorna

---
+++ src/Login/Hur-loggar-jag-in/Via-outlook-dator/index.php
@@ -0,0 +1,31 @@
+<?php
+  $header_title = "Via Outlook (Dator) - Hur loggar jag in? - Login";
+  $header_description = "Hur loggar jag in på intranätet (SharePoint) via Outlook (Dator)?";
+
+  $page_updated = "2022-12-31 19:24";
+  $page_url = "/Login/Hur-loggar-jag-in/Via-outlook-dator";
+  $page_contact_name = "Info";
+  $page_contact_email = "info@rockrullarna.se";
+
+  include_once '../../../includes/header.php'
+?>
+    <div id="BreadCrumbsDiv">
+      <a href="../../../">Rockrullarna.se</a> / <a href="../../">Login</a> / <a href="../">Hur loggar jag in?</a> / <span>Via Outlook (Dator)</span>
+    </div>
+    <h1>Hur loggar jag in via Outlook i en dator?</h1>
+    <p>Här nedan finner du en guide för hur du loggar in via Outlook i en dator (kräver dock att du har en egen licens för Outlook, privat eller via jobbet).</p>
+    <ol>
+      <li>Starta <strong>Outlook</strong>.&nbsp;</li>
+      <li>Klicka på <strong>Arkiv</strong>.</li>
+      <li>Gå på fliken&nbsp;<strong>Info</strong> sedan&nbsp;<strong>Lägg till konto</strong>.</li>
+      <li>Skriv in din e-postadress:&nbsp;<strong>fornamn.efternamn@rockrullarna.se</strong> , klicka på&nbsp;<strong>Anslut</strong>.</li>
+      <li>Skriv in ditt <strong>lösenord</strong>, och tryck på <strong>Logga in</strong>.</li>
+      <li>Godkänn begäran om inloggning via fler faktor autentisering.&nbsp;</li>
+      <li>Nu kan du trycka på&nbsp;<strong>Endast den här appen&nbsp;</strong>, eller godkänna med&nbsp;<strong>Ja</strong> , ifall du vill behålla inloggningen på din dator för även andra appar och liknande.&nbsp;</li>
+      <li>Kontot har lagts till i Outlook , tryck på&nbsp;<strong>Klar</strong>.</li>
+      <li>Stäng Outlook och starta det igen, nu kommer du åt din personliga mail.&nbsp;<br /><br />Nu kan du börja lägga till en delad brevlåda:&nbsp;<br /><br /></li>
+      <li>Info kommer...&nbsp;</li>
+    </ol>
+<?php
+  include_once '../../../includes/footer.php'
+?>
---
+++ src/Login/Hur-loggar-jag-in/Via-outlook-mobil/index.php
@@ -0,0 +1,43 @@
+<?php
+  $header_title = "Via Outlook (Mobil) - Hur loggar jag in? - Login";
+  $header_description = "Hur loggar jag in på intranätet (SharePoint) via Outlook (Mobil)?";
+
+  $page_updated = "2022-12-31 19:24";
+  $page_url = "/Login/Hur-loggar-jag-in/Via-outlook-mobil";
+  $page_contact_name = "Info";
+  $page_contact_email = "info@rockrullarna.se";
+
+  include_once '../../../includes/header.php'
+?>
+    <div id="BreadCrumbsDiv">
+      <a href="../../../">Rockrullarna.se</a> / <a href="../../">Login</a> / <a href="../">Hur loggar jag in?</a> / <span>Via Outlook (Mobil)</span>
+    </div>
+    <h1>Hur loggar jag in via Outlook i en mobil/surfplatta?</h1>
+    <p>Här nedan finner du en guide för hur du loggar in via Outlook-appen i en mobiltelefon.</p>
+    <ol>
+      <li>Ladda ned och installera <strong>Microsoft Outlook&nbsp;</strong>via din app butik (t.ex. Google Play eller Apple Appstore).&nbsp;</li>
+      <li>Starta appen&nbsp;<strong>Outlook</strong>.&nbsp;<br />( Använder du appen sedan tidigare behöver du först: )<br /><ol>
+      <li>Trycka på <strong>Inkorg&nbsp;</strong>/ din visningsbild&nbsp;uppe till vänster.&nbsp;</li>
+      <li>Tryck på <strong>+</strong><em>("plusset")</em> vid <em>brev-ikonen</em>.&nbsp;</li>
+      <li>Tryck på <strong>Lägg till e-postkonto</strong>.&nbsp;</li>
+      </ol></li>
+      <li>Skriv in din e-postadress:&nbsp;<strong>fornamn.efternamn@rockrullarna.se</strong> , samt tryck på&nbsp;<strong>Lägg till konto</strong></li>
+      <li>Du kan nu behöva autentisera dig via Autenticator appen om du har satt upp denna&nbsp;<br />(annars bör du få en ruta att skriva ditt lösenord direkt här).&nbsp;</li>
+      <li>Logga in med ditt lösenord , och godkänn 2 faktorinloggningen (antingen via SMS, privat e-post eller via en godkännande begäran via Authenticator appen).&nbsp;</li>
+      <li>Klart! Nu har du din privata @rockrullarna.se -adress i din mobil.&nbsp;<br /><br />Nu går vi vidare och lägger till de delade postlådorna du vill lägga upp i din mobil <br />(OBS. Det går bara att lägga till de delade postlådorna som du har behörighet till)&nbsp;<br /><br /></li>
+      <li>Tryck på&nbsp;<strong>Inkorg&nbsp;</strong>/ din visningsbild uppe till vänster.</li>
+      <li>Tryck på&nbsp;<strong style="font-style: italic;">+</strong><span style="font-style: italic;">&nbsp;</span><em>("plusset")</em> vid <em>brev-ikonen</em><span style="font-style: italic;">.&nbsp;</span></li>
+      <li>Tryck nu på&nbsp;<strong>Lägg till delad postlåda</strong>.&nbsp;</li>
+      <li>Skriv in den delade postlådan du vill lägga till, innan @rockrullarna.se <br />( t.ex: info, kurser, styrelsen, med mera.. Se hela listan via: <a href="/Login/Webbmail/" title="Webbmail" target="_blank">Webbmail</a>-sidan ) .</li>
+      <li>Tryck sedan på&nbsp;<strong>Lägg till delad postlåda</strong>.&nbsp;</li>
+      <li>Nu kommer du åt den delade postlådan också i appen på din mobil. <br />(Upprepa ifrån steg 7 i guiden, för att lägga till flera delade postlådor)&nbsp;<br /><br /><br /><br />Tips! Du kan ställa in&nbsp;<strong>Stör ej</strong> i appen så att du inte blir störd av Rockrullarna-mail under arbetstid eller kvällstid/helger, om du vill.&nbsp;</li>
+      </ol><ol>
+      <li>Tryck på&nbsp;<strong>Inkorg&nbsp;</strong>/ din visningsbild uppe till vänster.&nbsp;</li>
+      <li>Markera i vänsterkant det kontot du vill ändra inställningen för (kolla längst upp i mitten vilken mailadress som blir markerad).&nbsp;</li>
+      <li>Tryck sedan längst upp till höger på den "<strong>Ringklockan</strong>" som finns där.&nbsp;</li>
+      <li>Här kan du sedan ställa in flertalet olika schemalagda inställningar för <strong>Stör ej</strong>.&nbsp;</li>
+    </ol>
+    <p>&nbsp;</p>
+<?php
+  include_once '../../../includes/footer.php'
+?>
