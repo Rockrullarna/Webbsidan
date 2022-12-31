01ec67cabcda599e83a240a50a7eb33f663c2d5f
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2022-12-31 20:05:40 +0100

    Lagt till kursutvärderingen och word-wrap
    word-wrap, bryter längre ord som t.ex. långa länkar

---
+++ src/kursutvardering/index.php
@@ -0,0 +1,22 @@
+<?php
+  $header_title = "Kursutvärdering";
+  $header_description = "Kursutvärdering för kurser du gått hos Dansklubben Rockrullarna i Örebro";
+
+  $page_updated = "2022-12-31 19:58";
+  $page_url = "/Kursutvardering";
+  $page_contact_name = "Info";
+  $page_contact_email = "info@rockrullarna.se";
+
+  include_once '../includes/header.php'
+?>
+    <div id="BreadCrumbsDiv">
+      <a href="../">Rockrullarna.se</a> / <span>Kursutvärdering</span>
+    </div>
+    <h1>Kursutvärdering</h1>
+    <p class="word-wrap">Här kan du öppna kursutvärderingen i helskärm: <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=S-zWnBNPLE2jTQvbjZcqrrlQpz3qI7VEifDox3Ooy85UQ1hXNUVUWVJRV1pLVVBBM1RQUUU2NTRWSSQlQCN0PWcu" title="Kursutvärdering Rockrullarna" target="_blank">Kursutvärdering Rockrullarna</a></p>
+    <p><iframe frameborder="0" allowfullscreen="" height="5500" marginheight="0" marginwidth="0" src="https://forms.office.com/Pages/ResponsePage.aspx?id=S-zWnBNPLE2jTQvbjZcqrrlQpz3qI7VEifDox3Ooy85UQ1hXNUVUWVJRV1pLVVBBM1RQUUU2NTRWSSQlQCN0PWcu&amp;embed=true" style="border: none; max-width: 100%;" width="98%"></iframe></p>
+    <p>&nbsp;</p>
+    <p class="word-wrap">Direktlänk till kursutvärderings-formuläret (för att öppna i helskärm): <br /> <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=S-zWnBNPLE2jTQvbjZcqrrlQpz3qI7VEifDox3Ooy85UQ1hXNUVUWVJRV1pLVVBBM1RQUUU2NTRWSSQlQCN0PWcu" title="Kursutvärdering Rockrullarna" target="_blank">https://forms.office.com/Pages/ResponsePage.aspx?id=S-zWnBNPLE2jTQvbjZcqrrlQpz3qI7VEifDox3Ooy85UQ1hXNUVUWVJRV1pLVVBBM1RQUUU2NTRWSSQlQCN0PWcu</a></p>
+<?php
+  include_once '../includes/footer.php'
+?>
