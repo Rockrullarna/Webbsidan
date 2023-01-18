98edee86c7855806d54568db894a53524cf2dbd8
Jonas Arvidson <pownas@outlook.com>
2023-01-18 02:11:46 +0100

    Update footer.php

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -26,7 +26,7 @@
       </div>
       <div id="footer-mail" class="col-12 col-sm-6 col-lg-4 col-xl-3 text-center p-0 mt-4">
         KONTAKT<br />
-        Messenger: <a href="https://m.me/rockrullarna" title="Chatta med oss på Messenger" target="_blank">m.me/rockrullarna</a><br />
+        Messenger: <a href="https://m.me/rockrullarna" title="Chatta med oss på Messenger" target="_blank" rel="nofollow">m.me/rockrullarna</a><br />
         E-post: <a href="mailto:info@rockrullarna.se" title="Mejla till: info@rockrullarna.se">info@rockrullarna.se</a>
       </div>
       <div id="footer-social" class="col-12 col-lg-4 col-xl-3 text-center align-self-end p-0 mt-4 mb-3">
@@ -45,7 +45,7 @@
           </svg></a>
       </div>
       <div id="footer-contact" class="col-12 col-xl-3 text-center align-self-end p-0 mt-4 mb-3">
-        &copy; <?php echo date("Y"); ?> - Dansklubben Rockrullarna, v12.2.230118<br />
+        &copy; <?php echo date("Y"); ?> - Dansklubben Rockrullarna, <a title="Visa källkoden via GitHub" href="https://github.com/Rockrullarna/Webbsidan/tree/2bc1502" target="_blank" rel="nofollow">v12.2.230118</a><br />
         <a href="/Kontakt" title="Visa sidan med kontaktinformation">Se all kontaktinformation</a>
       </div>
     </div>
