99041fb0868c25940308da9f9592704239fb0091
Jonas Arvidson <pownas@outlook.com>
2023-01-27 11:42:48 +0100

    Buggfix-Råkade editera fel dokument med Å i sökväg

--- src/Foreningen/Styrande-dokument/Arshjul/index.php
+++ src/Foreningen/Styrande-dokument/Arshjul/index.php
@@ -2,7 +2,7 @@
   $header_title = "Årshjulet - Styrande dokument - Föreningen";
   $header_description = "DKRRs Årshjul används av föreningens medlemmar för att hålla koll på alla de årliga åtaganden vi har inom Rockrullarna";
 
-  $page_updated = "2022-12-29 23:28";
+  $page_updated = "2023-01-27 11:24";
   $page_url = "/Foreningen/Styrande-dokument/Arshjul";
   $page_contact_name = "Styrelsen";
   $page_contact_email = "styrelsen@rockrullarna.se";
@@ -17,11 +17,17 @@
     </h1>
     <p>Syftet med DKRR Årshjul är för att hjälpa våra medlemmar att hålla koll på alla de årliga åtaganden vi har inom Rockrullarna.</p>
     <p>Årshjulet ska ses över minst en gång per år och uppdateras vid behov.</p>
-    <p>Styrelsen tar tacksamt emot synpunkter på innehållet. <br />Synpunkter och frågeställningar skickas till: <a href="mailto:styrelsen@rockrullarna.se" title="Mejla till styrelsen@rockrullarna.se">styrelsen@rockrullarna.se</a></p>
-    <p>Öppna dokumentet i nytt fönster: <br />
-      <a href="DKRR-001-v1.0-Verksamhetsbeskrivning.pdf"><img src="/filer/bilder/design/extension-pdf.gif" width="18" height="18" /> DKRR-001-v1.0-Verksamhetsbeskrivning.pdf</a><br />
-      Eller läs det här nedan.</p>
-    <iframe src="DKRR-001-v1.0-Verksamhetsbeskrivning.pdf"  style="width: 98%; min-width: 320px; height: 600px"></iframe>
+    <p>
+      Styrelsen tar tacksamt emot synpunkter på innehållet.<br />
+      Synpunkter och frågeställningar skickas till: 
+      <a href="mailto:styrelsen@rockrullarna.se" title="Mejla till styrelsen@rockrullarna.se">styrelsen@rockrullarna.se</a>
+    </p>
+    <p>
+      Öppna dokumentet i nytt fönster: <br />
+      <a href="DKRR-002-v1.0-Årshjul.pdf"><img src="/filer/bilder/design/extension-pdf.gif" width="18" height="18" /> DKRR-002-v1.0-Årshjul.pdf</a><br />
+      Eller läs det här nedan.
+    </p>
+    <iframe src="DKRR-002-v1.0-Årshjul.pdf"  style="width: 98%; min-width: 320px; height: 600px"></iframe>
     <p>&nbsp;</p>
     <p>&nbsp;</p>
     <p>&nbsp;</p>
