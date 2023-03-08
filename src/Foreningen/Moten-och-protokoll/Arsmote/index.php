aac3dcd8174a6d71c1238465e159d0e7b601ee71
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2023-03-08 08:12:32 +0100

    Åsmöten - Uppdaterat sidan

--- src/Foreningen/Moten-och-protokoll/Arsmote/index.php
+++ src/Foreningen/Moten-och-protokoll/Arsmote/index.php
@@ -2,7 +2,7 @@
   $header_title = "Årsmöte - Möten och protokoll - Föreningen";
   $header_description = "Här finner du våra publika årsmötes-protokoll från olika möten vi genomfört";
 
-  $page_updated = "2022-12-30 00:28";
+  $page_updated = "2023-03-08 07:52";
   $page_url = "/Foreningen/Moten-och-protokoll/Arsmote";
   $page_contact_name = "Styrelsen";
   $page_contact_email = "styrelsen@rockrullarna.se";
@@ -19,15 +19,58 @@
     <p>Anmälan till årsmötet görs via sidan <a href="/Danskurser/anmalandanskurser/" title="Anmälan danskurser" target="_top">Anmälan danskurser</a> under kategorin "Årsmöte". </p>
     <p>Information gällande årsmötet kommer ut via sociala medier, hemsida samt via tränare. <br />Mer information gällande årsmötet hittats i <a href="/foreningen/Styrandedokument/Verksamhetsbeskrivning/" title="Verksamhetsbeskrivningen">Verksamhetsbeskrivningen</a> - Annex A (<a href="/foreningen/Styrandedokument/Stadgar/" title="Stadgarna">Stadgarna</a>).</p>
     <p>&nbsp;</p>
-    <h2>Protokollen för årsmöten</h2>
-    <p>Här finns protokoll från våra årsmöten. Sidan visas enklast och bäst via en dator, då mobilen vill öppna länken via OneDrive/SharePoint appen...</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <h2>Årsmötet 2023</h2>
+    <p>Anmälan till årsmötet 2023 gör du här: <a href="https://dans.se/rockrullarna/shop/new?event=180308" title="_blank">Anmälningsformulär till årsmötet 2023</a>.</p>
+    <h3>Handlingar inför Årsmötet 2023 (2022 års verksamhet)</h3>
+    <p>Här finns alla de handlingar som ska avhandlas inför årsmötet 2023, där vi går igenom verksamhetsåret 2022.</p>
+    <table class="table">
+      <tr>
+        <th>Handlingarna</th>
+        <th></th>
+      </tr>
+      <tr>
+        <td>DKRR-2023 Dagordning Årsmöte</td>
+        <td><a href="./Handlingar/DKRR-2023-Dagordning-Årsmöte.pdf" target="_blank">Visa / Ladda ned</a></td>
+      </tr>
+      <tr>
+        <td>DKRR-2023 Ekonomi - Balansräkning 2022-12-31</td>
+        <td><a href="./Handlingar/DKRR-2023-Ekonomi-Balansräkning--2022-12-31.pdf" target="_blank">Visa / Ladda ned</a></td>
+      </tr>
+      <tr>
+        <td>DKRR-2023 Ekonomi - Resultaträkning 2022-12-31</td>
+        <td><a href="./Handlingar/DKRR-2023-Ekonomi-Resultaträkning--2022-12-31.pdf" target="_blank">Visa / Ladda ned</a></td>
+      </tr>
+      <tr>
+        <td>DKRR-2023 Versamhetsberättelse för 2022</td>
+        <td><a href="./Handlingar/DKRR-2023-Versamhetsberättelse-för-2022.pdf" target="_blank">Visa / Ladda ned</a></td>
+      </tr>
+      <tr>
+        <td>DKRR-2023 Års Ekonomi Budget</td>
+        <td><a href="./Handlingar/DKRR-2023-Års-Ekonomi-Budget.pdf" target="_blank">Visa / Ladda ned</a></td>
+      </tr>
+      <tr>
+        <td>DKRR-2023 Revisionsberättelse</td>
+        <td><i>Kommer snart...</i></td>
+      </tr>
+      <tr>
+        <td>DKRR-2023 Valberedningens förslag</td>
+        <td><i>Kommer snart...</i></td>
+      </tr>
+    </table>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <h2>Arkiv för årsmötes protokoll och handlingar</h2>
+    <p>Via vår SharePoint (Microsoft365) så finns det arkiverat protokoll och handlingar från våra tidigare årsmöten. Sidan visas enklast och bäst via en dator, då mobilen vill öppna länken via OneDrive/SharePoint appen...</p>
     <p>
       <a href="https://rockrullarna.sharepoint.com/:f:/s/Protokoll/Esnw21kgySVGhlMpoWoxRo4BpkWyddlH5bix5ssyM5MQVw?e=Hpg6gG" target="_blank" title="Rockrullarnas årsmötesprotokoll (öppnas i nytt fönster)">
-        <span style="font-size: x-large;">Årsmötesprotokoll (Publikt på SharePoint)</span>
+        <span style="font-size: x-large;">Årsmötesprotokoll (Publik mapp på SharePoint)</span>
       </a>
     </p>
     <p class="word-wrap">
-      https://rockrullarna.sharepoint.com/sites/Protokoll/Delade%20dokument/Årsmöten (kräver inloggning)
+      <a href="https://rockrullarna.sharepoint.com/sites/Protokoll/Delade%20dokument/Årsmöten" target="_blank" title="Rockrullarnas protokoll-arkiv (öppnas i ny flik och kräver inloggning)">https://rockrullarna.sharepoint.com/sites/Protokoll/Delade%20dokument/Årsmöten</a> (kräver inloggning)
     </p>
 <?php
   include_once '../../../includes/footer.php'
