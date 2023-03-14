34b1ed6f8692fb3127e1b9597c57f8aceb1f6062
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2023-03-14 21:39:44 +0100

    Återkalla 2 rader från förra commit
    This reverts 2 mistakes in commit 81582352a6f0b9b2bdb94d7daa99387ff3b2329a.

--- src/includes/header.php
+++ src/includes/header.php
@@ -7,7 +7,7 @@
   <link href="/filer/bilder/Rockrullarna-favicon.png?w=32" rel="shortcut icon" type="image/x-icon">
   <!-- Bootstrap Theme switcher --><script src="/filer/js/bootstrap-theme-switcher.js"></script>
   <!-- Bootstrap 5 CDN Links --><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <link rel="stylesheet" href="/filer/css/rockrullarna-v12.5.20230314.css" /> 
+  <link rel="stylesheet" href="/filer/css/rockrullarna.css" /> 
   <title><?php if (empty($header_title)) {
     echo "Dansklubben Rockrullarna | Välkommen till vår ideella dansförening i Örebro!";
   } else {
@@ -112,7 +112,7 @@
     <nav class="navbar fixed-top navbar-expand-lg p-0">
       <div class="container-fluid dkrr-navbar-bg">
         <a class="navbar-brand dkrr-logo-link" href="/">
-          <img class="dkrr-logo" alt="Dansklubben Rockrullarna" src="/filer/bilder/Rockrullarna-SVG-logga-v12.5.20230314.svg" />
+          <img class="dkrr-logo" alt="Dansklubben Rockrullarna" src="/filer/bilder/Rockrullarna-SVG-logga.svg" />
         </a>
         <button class="navbar-toggler collapsed" title="Visa menyn" aria-label="Visa menyn" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-content">
           <span class="navButton-Line" aria-hidden="true"></span>
