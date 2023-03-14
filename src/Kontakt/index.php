2306d1449bbc3c453fe9f50ad87241634d3fa027
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2023-03-14 18:07:08 +0100

    Uppdaterat kontaktsidan

--- src/Kontakt/index.php
+++ src/Kontakt/index.php
@@ -2,7 +2,7 @@
   $header_title = "Kontakta oss";
   $header_description = "Kontaktinformation till den ideella föreningen Dansklubben Rockrullarna i Örebro";
 
-  $page_updated = "2022-12-29 23:41";
+  $page_updated = "2023-03-14 18:01";
   $page_url = "/Kontakt";
   $page_contact_name = "Info";
   $page_contact_email = "info@rockrullarna.se";
@@ -44,71 +44,63 @@
       </tbody>
     </table>
     <hr />
-    <h2>DK Rockrullarna - Danssport</h2>
-    <table class="StatisticsTable" cellspacing="20" summary="Uppgifter">
-      <tbody>
-        <tr>
-            <td class="LeftAlignTd widthTen"><strong>Bildad</strong></td>
-            <td class="LeftAlignTd widthNinety">1983-10-23</td>
-        </tr>
-
-        <tr>
-            <td class="LeftAlignTd widthTen"><strong>Föreningsnummer</strong></td>
-            <td class="LeftAlignTd widthNinety">20550-55</td>
-        </tr>
-
-        <tr>
-            <td class="LeftAlignTd widthTen"><strong>Postadress</strong></td>
-            <td class="LeftAlignTd widthNinety">Vaktelvägen 2 <br /> 70348&nbsp;Örebro </td>
-        </tr>
-
-        <tr>
-            <td class="LeftAlignTd widthTen"><strong>Besöksadress</strong></td>
-            <td class="LeftAlignTd widthNinety">Vaktelvägen 2, Haga Centrum <br /> 70348&nbsp;Örebro </td>
-        </tr>
-
-        <tr>
-            <td class="LeftAlignTd widthTen"><strong>Öppettider</strong></td>
-            <td class="LeftAlignTd widthNinety">mån-tors 18-21, sön 16-21</td>
-        </tr>
-
-        <tr>
-            <td class="LeftAlignTd widthTen"><strong>Sommartid</strong></td>
-            <td class="LeftAlignTd widthNinety">stängt</td>
-        </tr>
-
-        <tr>
-            <td class="LeftAlignTd widthTen"><strong>E-post</strong></td>
-            <td class="LeftAlignTd widthNinety"><a href="mailto:info@rockrullarna.se" title="info@rockrullarna.se">info@rockrullarna.se</a></td>
-        </tr>
-
-        <tr>
-            <td class="LeftAlignTd widthTen"><strong>Faktura e-post</strong></td>
-            <td class="LeftAlignTd widthNinety"><a href="mailto:ekonomi@rockrullarna.se" title="ekonomi@rockrullarna.se">ekonomi@rockrullarna.se</a></td>
-        </tr>
-
-        <tr>
-            <td class="LeftAlignTd widthTen"><strong>Hemsida</strong></td>
-            <td class="LeftAlignTd widthNinety"><a href="https://www.rockrullarna.se">www.rockrullarna.se</a></td>
-        </tr>
-
-        <tr>
-            <td class="LeftAlignTd widthTen"><strong>Bankgiro</strong></td>
-            <td class="LeftAlignTd widthNinety">865-0277</td>
-        </tr>
-
-        <tr>
-            <td class="LeftAlignTd widthTen"><strong>Swish</strong></td>
-            <td class="LeftAlignTd widthNinety">1232220283</td>
-        </tr>
-
-        <tr>
-            <td class="LeftAlignTd widthTen"><strong>Org. nummer</strong></td>
-            <td class="LeftAlignTd widthNinety">875001-4147</td>
-        </tr>
-      </tbody>
-    </table>
-    <hr />
+    <h2>Kontaktuppgifter för DK Rockrullarna - Danssport</h2>
+    <div class="row">
+      <div class="table-responsive col-auto">
+        <table class="table table-dark table-striped" summary="Kontaktuppgifter">
+          <tbody>
+            <tr>
+              <th width="6em">Bildad</th>
+              <td>1983-10-23</td>
+            </tr>
+            <tr>
+              <th>Föreningsnummer</th>
+              <td>20550-55</td>
+            </tr>
+            <tr>
+              <th>Postadress</th>
+              <td>Vaktelvägen 2 <br /> 70348 Örebro </td>
+            </tr>
+            <tr>
+              <th>Besöksadress</th>
+              <td>Vaktelvägen 2, Haga Centrum <br /> 70348 Örebro </td>
+            </tr>
+            <tr>
+              <th>Öppettider</th>
+              <td>mån-tors 18-21, sön 16-21</td>
+            </tr>
+            <tr>
+              <th>Sommartid</th>
+              <td>stängt</td>
+            </tr>
+            <tr>
+              <th>E-post</th>
+              <td><a href="mailto:info@rockrullarna.se" title="info@rockrullarna.se">info@rockrullarna.se</a></td>
+            </tr>
+            <tr>
+              <th>Faktura e-post</th>
+              <td><a href="mailto:ekonomi@rockrullarna.se" title="ekonomi@rockrullarna.se">ekonomi@rockrullarna.se</a></td>
+            </tr>
+            <tr>
+              <th>Hemsida</th>
+              <td><a href="https://www.rockrullarna.se">www.rockrullarna.se</a></td>
+            </tr>
+            <tr>
+              <th>Bankgiro</th>
+              <td>865-0277</td>
+            </tr>
+            <tr>
+              <th>Swish</th>
+              <td>1232220283</td>
+            </tr>
+            <tr>
+              <th>Org. nummer</th>
+              <td>875001-4147</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
     <h2>Hitta till oss</h2>
     <p><strong>► <a href="https://www.google.se/maps/place/Rockrullarna+i+%C3%96rebro/@59.2747154,15.1734813,14.12z/data=!4m5!3m4!1s0x465c14d4a35b37db:0x948d71326b2d8b7c!8m2!3d59.2754033!4d15.1647323" title="Hitta till oss (Google Maps)">Visa Google Maps karta (öppnas i nytt fönster)</a></strong></p>
     <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d460.6115304119815!2d15.164433639652334!3d59.27572964011082!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465c14d4a35b37db%3A0x948d71326b2d8b7c!2sDansklubben%20Rockrullarna%20i%20%C3%96rebro!5e0!3m2!1ssv!2sse!4v1672057129075!5m2!1ssv!2sse" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></p>
