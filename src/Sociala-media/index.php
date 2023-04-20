ef14ba4c934904700d186b6a63ebfc44ca20c712
Jonas Arvidson <pownas@outlook.com>
2023-04-20 21:39:29 +0200

    Uppdaterat sidan Sociala media

--- src/Sociala-media/index.php
+++ src/Sociala-media/index.php
@@ -2,7 +2,7 @@
   $header_title = "Sociala media flöden";
   $header_description = "Information om de sociala media-sidorna vi använder oss av för kommunikation";
 
-  $page_updated = "2022-12-30 01:15";
+  $page_updated = "2023-04-20 21:38";
   $page_url = "/Sociala-media";
   $page_contact_name = "Info";
   $page_contact_email = "info@rockrullarna.se";
@@ -16,7 +16,9 @@
     <p>
       Här finns information från våra sociala media.
     </p>
-    <h2>Facebook</h2>
+    <h2>
+      <svg width="16" height="16" fill="currentColor" class="bi bi-facebook"><use href="#facebook"></use></svg> Facebook
+    </h2>
     <p>
       <a href="https://fb.me/rockrullarna" title="Rockrullarna på facebook">fb.me/rockrullarna</a>
     </p>
@@ -24,11 +26,15 @@
       <!-- Feed uppdateras från: https://developers.facebook.com/docs/plugins/page-plugin/ -->
       <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Frockrullarna&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=702771861196793" width="500" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
     </p>
-    <h2>Instagram</h2>
+    <h2>
+      <svg width="16" height="16" fill="currentColor" class="bi bi-tiktok"><use href="#instagram"></use></svg> Instagram
+    </h2>
     <p>
       <a href="https://www.instagram.com/rockrullarna" title="Rockrullarna på instagram">instagram.com/rockrullarna</a>
     </p>
-    <h2>Tik-Tok</h2>
+    <h2>
+      <svg width="16" height="16" fill="currentColor" class="bi bi-tiktok"><use href="#tiktok"></use></svg> Tik-Tok
+    </h2>
     <p><a href="https://www.tiktok.com/@dansklubbrockrullarna" title="Rockrullarna på tik tok">tiktok.com/@dansklubbrockrullarna</a></p>
     <p>
       <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@dansklubbrockrullarna" data-unique-id="dansklubbrockrullarna" data-embed-type="creator" style="max-width: 780px; min-width: 288px;" > <section> <a target="_blank" href="https://www.tiktok.com/@dansklubbrockrullarna?refer=creator_embed">@dansklubbrockrullarna</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>
