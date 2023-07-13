38d0c4dc837b0073bcd4fcab3a6a7840ee88442d
Jonas Arvidson <47676607+pownas@users.noreply.github.com>
2023-07-13 22:18:08 +0200

    Bootstrap 5.3 - Uppdatering
    Uppdaterar till fullversionen av Bootstrap v5.3 istället för 5.3-alpha versionen

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -79,7 +79,7 @@
         <a href="https://rockrullarna.se/Kontakt" title="Visa sidan med kontaktinformation">Se all kontaktinformation</a>
       </div>
     </div>
-    <!-- Bootstrap 5 CDN Links --><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <!-- Bootstrap 5 CDN Links --><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   </footer>
 </body>
 </html>
