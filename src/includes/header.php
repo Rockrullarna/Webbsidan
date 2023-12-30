1e13a09014a3b07acb6419a49311bcaa06bd0c6f
Jonas Arvidson <pownas@outlook.com>
2023-12-30 16:11:42 +0100

    Uppdaterat menyn med information om dans.se och idrottonline

--- src/includes/header.php
+++ src/includes/header.php
@@ -212,7 +212,12 @@
                   </ul>
                 </li>
                 <li><a class="dropdown-item" href="https://rockrullarna.se/Foreningen/Medlemsrabatter">Medlemsrabatter</a></li>
-                <li><a class="dropdown-item" href="https://dans.se/shop/?org=rockrullarna&mship" title="Bli medlem hos oss (Öppnas i nytt fönster)" target="_blank" rel="noopener">Bli medlem hos oss (dans.se)</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="https://dans.se/" title="Medlemsregister (dans.se - Öppnas i nytt fönster)" target="_blank" rel="noopener">Medlemsregister (dans.se)</a></li>
+                <li><a class="dropdown-item" href="https://www.rf.se/bidrag-och-stod/it-tjanster/idrottonline/" title="Information om IdrottOnline hos Riksidrottsförbundet (rf.se - Öppnas i nytt fönster)" target="_blank" rel="noopener">IdrottOnline information (rf.se)</a></li>
+                <li><a class="dropdown-item" href="https://login.idrottonline.se/" title="IdrottOnline inloggningssida (login.idrottonline.se - Öppnas i nytt fönster)" target="_blank" rel="noopener">IdrottOnline (login.idrottonline.se)</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="https://dans.se/shop/?org=rockrullarna&mship" title="Bli medlem hos Rockrullarna (dans.se - Öppnas i nytt fönster)" target="_blank" rel="noopener">Bli medlem hos Rockrullarna</a></li>
               </ul>
             </li>
             <li class="nav-item dropdown">
@@ -220,7 +225,7 @@
               <ul class="dropdown-menu shadow">
                 <li><a class="dropdown-item" href="https://rockrullarna.se/Kontakt">Kontaktinformation</a></li>
                 <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="https://www.google.se/maps/place/Rockrullarna+i+%C3%96rebro/@59.2747154,15.1734813,14.12z/data=!4m5!3m4!1s0x465c14d4a35b37db:0x948d71326b2d8b7c!8m2!3d59.2754033!4d15.1647323" title="Hitta till oss (Google maps öppnas i nytt fönster)" target="_blank" rel="noopener">Hitta till oss (Google maps)</a></li>
+                <li><a class="dropdown-item" href="https://www.google.se/maps/place/Rockrullarna+i+%C3%96rebro/@59.2747154,15.1734813,14.12z/data=!4m5!3m4!1s0x465c14d4a35b37db:0x948d71326b2d8b7c!8m2!3d59.2754033!4d15.1647323" title="Hitta till oss (Google maps - Öppnas i nytt fönster)" target="_blank" rel="noopener">Hitta till oss (Google maps)</a></li>
                 <li><hr class="dropdown-divider"></li>
                 <li class="dropstart">
                   <a href="#" class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside">Frågor och Svar</a>
