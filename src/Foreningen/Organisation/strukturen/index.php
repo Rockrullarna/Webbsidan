cf88648cfac7db5804582d5a3ad2662f74636f74
Jonas Arvidson <pownas@outlook.com>
2024-06-09 11:54:50 +0200

    Lagt till lite info om organisatonsstrukturen

---
+++ src/Foreningen/Organisation/strukturen/index.php
@@ -0,0 +1,117 @@
+<?php
+$header_title = "Styrelsen - Organisation - Organisationsstruktur";
+$header_description = "Information om den ideella dansföreningen Rockrullarnas styrelse";
+
+$page_updated = "2024-06-09 11:34";
+$page_url = "/foreningen/organisation/styrelsen";
+$page_contact_name = "Styrelsen";
+$page_contact_email = "styrelsen@rockrullarna.se";
+
+include_once '../../../includes/header.php'
+?>
+  <div id="BreadCrumbsDiv">
+    <a href="../../../">Rockrullarna.se</a> / <a href="../../">Föreningen</a> / <a href="../">Organisation</a> / <span>Strukturen</span>
+  </div>
+  <h1>
+    Information om ny organisationsstruktur
+  </h1>
+  <p>
+    Vi är i startgroparna av en ny organisationsstruktur inom föreningen. 
+    Ansvariga och drivande personer inom våra respektive danssektioner, 
+    Bugg, Fox, WCS och Ungdom, kommer att ingå i kommittéer (arbetsgrupper) 
+    för sin respektive danssektion, enligt belsut på 2024 års årsmöte. 
+    En sektion för 65+ tittas på. Utbildning i styrelsearbete planeras.
+  </p>
+  <p>
+    Bakgrunden till denna nya organisationsstruktur är att det tidigare 
+    tillvägagångssättet visat sig skört. Ansvars har fallit på enskilda 
+    personer som ibland haft flera uppdrag som den varit tvungen att driva 
+    enskilt. När en person slutat så har avgörande positioner stått vakanta 
+    och verksamheten drabbats. Tanken med kommitté-strukturen är att en 
+    person inte ensam ska behöva sitta på ett uppdrag utan att man alltid 
+    är en grupp av flera personer som hjälps åt. Det ska inte vara betungande 
+    att vara engagerad hos oss, tvärtom! 
+  </p>
+  <p>
+    <a href="organisationstradet.jpg" title="Klicka för att se bild i större format">
+      <img src="organisationstradet.jpg" alt="En bild som visar Rockrullarnas organisationsträd" class="bild" />
+    </a>
+  </p>
+  <p>
+    <em>
+      Bilden visar Rockrullarnas nya organisationsstruktur. <br />
+      <i>Klicka på bilden för att se den i större format.</i>
+    </em>
+  </p>
+  <p>
+    <b>Kommittéernas uppdrag</b> är att vara en samlad kompetens inom sin dansstil 
+    i föreningen och att bl.a. bidra i föreningens verksamhetsplanering. 
+    De ska ha kunskap om bl.a. nyheter och events i närområdet under året 
+    inom sin dansstil. En kommitté ska ha en samordnande funktion för, och 
+    ett mycket nära samarbete med, övriga engagerade medlemmar inom danssektionen. 
+    Danssektionernas kommittéer ska samarbeta med varandra och en representant 
+    ifrån varje kommitté (roterande eller fast) ska delta på varje styrelsemöte. 
+    Detta innebär mer direktkontakt - mellan kommittéerna och styrelsen, samt 
+    kommittéerna emellan. 
+  </p>
+  <p>
+    <a href="sektionernas-koppling-till-styrelsen.jpg" title="Klicka för att se bild i större format">
+      <img 
+        src="sektionernas-koppling-till-styrelsen.jpg" 
+        alt="En bild som visar hur sektionerna kopplas till styrelsearbetet inom Rockrullarna"
+        class="bild" />
+    </a>
+  </p>
+  <p>
+    <em>
+      Bilden visar hur sektionerna kopplas till styrelsearbetet inom Rockrullarna. <br />
+      <i>Klicka på bilden för att se den i större format.</i>
+    </em>
+  </p>
+  <p>
+    Varje kommitté ska arbeta som ett team i alla frågor, och alla i kommittén ska 
+    kunna ta vid för varandra. Men de kommer att ha utsedda ansvariga inom olika 
+    områden som marknadsföring, tävling och danskvällar. Dessa områdesansvariga 
+    samarbetar med motsvarande områdesansvariga inom de andra kommittéerna, och bildar 
+    områdes-kommittéer (t.ex. marknadsföringskommittén). Avgränsade ansvarsområden kan 
+    även tilldelas andra medlemmar utanför kommittén. Roller som sammanlänkar områdes-kommittéer, 
+    verksamheten och styrelsen är bl.a. huvudtränaransvarig, kursadministratör, 
+    samordnande marknadsföringsansvarig och kommunikatör, dessa roller innehas ofta av personer
+    som redan sitter på andra ansvarsroller och har nära kontakt med verksamheten.
+  </p>
+  <p>
+    <a href="omrades-kommitteer.jpg" title="Klicka för att se bild i större format">
+      <img 
+        src="omrades-kommitteer.jpg" 
+        alt="En bild som visar hur ansvarsroller för tävling, är kopplad till gemensamma områdes kommittén Tävlings-kommittén"
+        class="bild" />
+    </a>
+  </p>
+  <p>
+    <em>
+      Bilden visar hur danssektionernas kommittéer har gemensamma områdes kommittéer. <br />
+      <i>Klicka på bilden för att se den i större format.</i>
+    </em>
+  </p>
+  <h2>
+    Medlemmar inom de olika dans-kommittéerna
+  </h2>
+  <h3>Ungdomskommittén</h3>
+  <p>Nellie Modin, Emma Elfström.</p>
+  <h3>Bugg-kommittén</h3>
+  <p>Stefan Ericsson, Joakim Berg, Linnea Sohlberg Modin, Thomas Modin.</p>
+  <h3>Fox-kommittén</h3>
+  <p>Jens Wiklund, Milla Olsson, Ulrika Bertilsdotter, Jonas Arvidson, Åsa Nyström.</p>
+  <h3>West Coast Swing-kommittén</h3>
+  <p>Sara Sjöström, Jonas Arvidson, Elin Östlund, Elin Nordeman <i>(tävling)</i>, Regina Holmberg <i>(danskvällar)</i>.</p>
+  <p>
+    <em>
+      Vid frågor, kontakta gärna oss i styrelsen via vår gemensamma e-post: 
+      <a href="mailto:styrelsen@rockrullarna.se" title="Mejla till styrelsen@rockrullarna.se">
+        styrelsen@rockrullarna.se
+      </a>
+    </em>
+  </p>
+<?php
+  include_once '../../../includes/footer.php'
+?>
