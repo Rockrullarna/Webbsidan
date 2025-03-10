aea26cc72a75944ef2f92ad4fc023167495e4cf2
Jonas Arvidson <pownas@outlook.com>
2025-03-10 19:18:15 +0100

    Uppdaterat flera felaktiga länkar

--- src/foreningen/styrande-dokument/index.php
+++ src/foreningen/styrande-dokument/index.php
@@ -2,7 +2,7 @@
   $header_title = "Styrande dokument - Föreningen";
   $header_description = "Våra styreande föreningsdokument";
 
-  $page_updated = "2023-01-27 11:28";
+  $page_updated = "2025-03-10 17:54";
   $page_url = "/foreningen/styrande-dokument";
   $page_contact_name = "Styrelsen";
   $page_contact_email = "styrelsen@rockrullarna.se";
@@ -20,32 +20,32 @@
       <tbody>
       <tr>
         <td>
-          <p><strong>► <a href="./Verksamhetsbeskrivning" title="DKRR-001 Verksamhetsbeskrivning">Verksamhetsbeskrivning</a></strong></p>
+          <p><strong>► <a href="./verksamhetsbeskrivning" title="DKRR-001 Verksamhetsbeskrivning">Verksamhetsbeskrivning</a></strong></p>
         </td>
       </tr>
       <tr>
         <td>
-          <p><strong>► <a href="./Stadgar" title="Stadgar">Stadgar</a></strong></p>
+          <p><strong>► <a href="./stadgar" title="Stadgar">Stadgar</a></strong></p>
         </td>
       </tr>
       <tr>
         <td>
-          <p><strong>► <a href="./Blanketter" title="Blanketter">Blanketter</a></strong></p>
+          <p><strong>► <a href="./blanketter" title="Blanketter">Blanketter</a></strong></p>
         </td>
       </tr>
       <tr>
         <td>
-          <p><strong>► <a href="./Integritetspolicy" title="Integritetspolicy">Integritetspolicy</strong></a></p>
+          <p><strong>► <a href="./integritetspolicy" title="Integritetspolicy">Integritetspolicy</strong></a></p>
         </td>
       </tr>
       <tr>
         <td>
-          <p><strong>► <a href="./Policy-mot-diskriminering" title="Policy mot diskriminering">Policy mot diskriminering</a></strong></p>
+          <p><strong>► <a href="./policy-mot-diskriminering" title="Policy mot diskriminering">Policy mot diskriminering</a></strong></p>
         </td>
       </tr>
       <tr>
         <td>
-          <p><strong>► <a href="./Arshjul" title="DKRR-002 Årshjul">Årshjul</a></strong></p>
+          <p><strong>► <a href="./arshjul" title="DKRR-002 Årshjul">Årshjul</a></strong></p>
         </td>
       </tr>
       </tbody>
