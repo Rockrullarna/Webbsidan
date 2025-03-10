3b94448c43d24aadbc4fc50b33349d83c65be048
Jonas Arvidson <pownas@outlook.com>
2025-03-10 19:18:31 +0100

    Uppdaterat sidan för valberedningen

--- src/foreningen/organisation/valberedningen/index.php
+++ src/foreningen/organisation/valberedningen/index.php
@@ -18,7 +18,16 @@
     <p>Valberednings arbetsuppgift är att ge förslag till revisorer och ledamöter till styrelsen. Som representant i valberedningen förväntas du ha god kunskap om verksamheten samt kännedom om de personer som är aktiva i föreningen.</p>
     <p>Valberedningen är fristående från styrelsen, ska kontinuerligt följa verksamheten och söka efter personer som tillsammans utger en bra representation av föreningens medlemmar.</p>
     <p>&nbsp;</p>
+    <h2>Valberedningen 2024-2025</h2>
+    <p>Valberedningen för mars 2024 - mars 2025 består av:</p>
+    <ul>
+      <li>Sofia Olsen</li>
+      <li>Anders Nilsson</li>
+      <li>Joakim Berg</li>
+    </ul>
+    <p>&nbsp;</p>
     <hr />
+    <h2>Förslag på nominering till styrelsen?</h2>
     <p>Har du förslag på personer som vill vara verksam i styrelsen? Skicka ditt förslag till valberedningen.</p>
     <p>► <a href="mailto:valberedningen@rockrullarna.se" title="Mejla till: valberedningen@rockrullarna.se">valberedningen@rockrullarna.se</a></p>
     <p><em>OBS. Personen du vill föreslå måste vara tillfrågad och vill ställa upp för val till styrelsen.</em></p>
