f870c3e58f05c30187acd70da6e0d6c2757c5f60
Jonas Arvidson <pownas@outlook.com>
2025-03-22 17:19:22 +0100

    Justerat länkar på undersidan för 2025

--- src/foreningen/moten-och-protokoll/arsmote/2025/index.php
+++ src/foreningen/moten-och-protokoll/arsmote/2025/index.php
@@ -10,7 +10,7 @@
   include_once '../../../../includes/header.php'
 ?>
     <div id="BreadCrumbsDiv">
-      <a href="../../../../">Rockrullarna.se</a> / <a href="../../../">Föreningen</a> / <a href="../../">Möten och protokoll</a> / <a href="../">Årsmöte</a> / <span>2024</span>
+      <a href="../../../../">Rockrullarna.se</a> / <a href="../../../">Föreningen</a> / <a href="../../">Möten och protokoll</a> / <a href="../">Årsmöte</a> / <span>2025</span>
     </div>
     <h1>
       Årsmöte 2025
@@ -20,47 +20,63 @@
     <table class="table">
       <tr>
         <th>Handlingarna för 2025</th>
-        <th></th>
+        <th>Dokument-länk</th>
       </tr>
       <tr>
         <td>DKRR-2025 Dagordning Årsmöte</td>
         <!-- <td><i>Kommer snart...</i></td> -->
-        <td><a href="./handlingar/2025/DKRR-2025-Dagordning-Årsmöte.pdf" target="_blank">Visa / Ladda ned</a></td>
+        <td><a href="../handlingar/2025/DKRR-2025-Dagordning-Årsmöte.pdf" target="_blank">Visa / Ladda ned</a></td>
       </tr>
       <tr>
         <td>DKRR-2025 Ekonomi - Balansräkning 2024-12-31</td>
         <!-- <td><i>Kommer snart...</i></td> -->
-        <td><a href="./handlingar/2025/DKRR-2025-Ekonomi-Balansräkning--2024-12-31.pdf" target="_blank">Visa / Ladda ned</a></td>
+        <td><a href="../handlingar/2025/DKRR-2025-Ekonomi-Balansräkning--2024-12-31.pdf" target="_blank">Visa / Ladda ned</a></td>
       </tr>
       <tr>
         <td>DKRR-2025 Ekonomi - Resultaträkning 2024-12-31</td>
         <!-- <td><i>Kommer snart...</i></td> -->
-        <td><a href="./handlingar/2025/DKRR-2025-Ekonomi-Resultaträkning--2024-12-31.pdf" target="_blank">Visa / Ladda ned</a></td>
+        <td><a href="../handlingar/2025/DKRR-2025-Ekonomi-Resultaträkning--2024-12-31.pdf" target="_blank">Visa / Ladda ned</a></td>
+      </tr>
+      <!-- <tr>
+        <td>DKRR-2025 Ekonomi - Årsbudget förslag 2025</td>
+        <td><i>Finns i Verksamhetsplanen</i></td>
+        <td><i>Kommer snart...</i></td>
+        <td><a href="../handlingar/2025/DKRR-2025-Ekonomi-Årsbudget-2025.pdf" target="_blank">Visa / Ladda ned</a></td>
+      </tr> -->
+      <tr>
+        <td>DKRR-2025 Motion 1 - Ekonomigruppering</td>
+        <!-- <td><i>Kommer snart...</i></td> -->
+        <td><a href="../handlingar/2025/DKRR-2025-Motion-1-Ekonomigruppering.pdf" target="_blank">Visa / Ladda ned</a></td>
       </tr>
       <tr>
-        <td>DKRR-2025 Ekonomi - Årsbudget förslag 2025</td>
-        <td><i>Kommer snart...</i></td>
-        <!-- <td><a href="./handlingar/2025/DKRR-2025-Ekonomi-Årsbudget-2025.pdf" target="_blank">Visa / Ladda ned</a></td> -->
+        <td>DKRR-2025 Motion 2 - Stadgeändring</td>
+        <!-- <td><i>Kommer snart...</i></td> -->
+        <td><a href="../handlingar/2025/DKRR-2025-Motion-2-Stadgeändring.pdf" target="_blank">Visa / Ladda ned</a></td>
+      </tr>
+      <tr>
+        <td>DKRR-2025 Motion 3 - Trygga idrottsmiljöer</td>
+        <!-- <td><i>Kommer snart...</i></td> -->
+        <td><a href="../handlingar/2025/DKRR-2025-Motion-3-Trygga-idrottsmiljöer.pdf" target="_blank">Visa / Ladda ned</a></td>
       </tr>
       <tr>
         <td>DKRR-2025 Revisionsberättelse för 2024</td>
         <!-- <td><i>Kommer snart...</i></td> -->
-        <td><a href="./handlingar/2025/DK-Rockrullarna-2024-Rev.berattelse-2024.pdf" target="_blank">Visa / Ladda ned</a></td>
+        <td><a href="../handlingar/2025/DKRR-2025-Revisionsberättelse-2024.pdf" target="_blank">Visa / Ladda ned</a></td>
       </tr>
       <tr>
         <td>DKRR-2025 Valberedningens förslag</td>
         <!-- <td><i>Kommer snart...</i></td> -->
-        <td><a href="./handlingar/2025/DKRR-2025-Valberedningens-förslag.pdf" target="_blank">Visa / Ladda ned</a></td>
+        <td><a href="../handlingar/2025/DKRR-2025-Valberedningens-förslag.pdf" target="_blank">Visa / Ladda ned</a></td>
       </tr>
       <tr>
-        <td>DKRR-2025 Versamhetsberättelse för 2024</td>
+        <td>DKRR-2025 Verksamhetsberättelse för 2024</td>
         <td><i>Kommer snart...</i></td>
-        <!-- <td><a href="./handlingar/2025/DKRR-2025-Versamhetsberättelse-för-2024.pdf" target="_blank">Visa / Ladda ned</a></td>  -->
+        <!-- <td><a href="../handlingar/2025/DKRR-2025-Verksamhetsberättelse-för-2024.pdf" target="_blank">Visa / Ladda ned</a></td>  -->
       </tr>
       <tr>
-        <td>DKRR-2025 Verksamhetsplan för 2025</td>
+        <td>DKRR-2025 Verksamhetsinriktning 2025-2026</td>
         <!-- <td><i>Kommer snart...</i></td> -->
-        <td><a href="./handlingar/2025/DKRR-2025-Verksamhetsplan.pdf" target="_blank">Visa / Ladda ned</a></td>
+        <td><a href="../handlingar/2025/DKRR-2025-Verksamhetsinriktning-2025-2026.pdf" target="_blank">Visa / Ladda ned</a></td>
       </tr>
     </table>
     <p>&nbsp;</p>
