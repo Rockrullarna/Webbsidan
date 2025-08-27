64ebf7260b0a1dd1186e266a92be615cf519a8fe
Jonas Arvidson <pownas@outlook.com>
2025-08-27 23:40:13 +0200

    Add marketing materials and index page
    Added flyers and course offering documents in PDF and DOCX formats to the marketing directory. Created index.php to display and link to these materials for easy access.

---
+++ src/marknadsforing/index.php
@@ -0,0 +1,58 @@
+<?php
+  $header_title = "Marknadsföring";
+  $header_description = "Flyers och annat som kan användas till marknadsföring";
+
+  $page_updated = "2025-08-27 23:25";
+  $page_url = "/marknadsforing";
+  $page_contact_name = "Info";
+  $page_contact_email = "info@rockrullarna.se";
+
+  include_once '../includes/header.php'
+?>
+  <div id="BreadCrumbsDiv">
+    <a href="../">Rockrullarna.se</a> / <span>Marknadsföring</span>
+  </div>
+  <h1>Rockrullarnas Marknadsföring</h1>
+  <p>Denna sidan innehåller material för marknadsföring som vi har på Rockrullarna.</p>
+  <p>Här kan du hämta ned flyers, affischer och annat material för att marknadsföra våra evenemang.&nbsp;</p>
+  <p>Du finner annan information om vår logga, färgkoder, design och typsnitt via sidan <a href="/logotyp/">Logotyp (loggan)</a>.</p>
+  <p>Direktlänk till denna sidan att dela:<br /><a href="/marknadsforing/">https://www.Rockrullarna.se/marknadsforing</a><em><br /></em></p>
+  <p>Här kan du sortera på kategori:</p>
+  
+  <div class="table-responsive">
+    <table class="GalleryTable table table-secondary table-striped">
+      <tr class="GalleryTableHeader">
+        <td colspan="2">Titel</td>
+        <td>Uppladdad</td>
+        <td>Filtyp</td>
+        <td>Storlek</td>
+      </tr>
+
+      <tr>
+        <td colspan="2" class="GalleryTableCellIcon">
+          <a href="./format/PDF/DKRR-flyers.pdf" title="DKRR-flyers.pdf" target="_self" rel="lightbox-gallery">
+            <img src="/filer/bilder/design/extension-pdf.gif" width="18" height="18" />&nbsp;
+            DKRR-flyers
+          </a> 
+        </td>
+        <td class="GalleryTableCellFileDate">2025-08-27</td>
+        <td class="GalleryTableCellFileType">PDF</td>
+        <td class="GalleryTableCellFileSize">107 kB</td>
+      </tr>
+
+      <tr>
+        <td colspan="2" class="GalleryTableCellIcon">
+          <a href="./format/PDF/DKRR-kursutbud-uppvisning.pdf" title="DKRR-kursutbud-uppvisning.pdf" target="_self" rel="lightbox-gallery">
+            <img src="/filer/bilder/design/extension-pdf.gif" width="18" height="18" />&nbsp;
+            DKRR-kursutbud-uppvisning
+          </a> 
+        </td>
+        <td class="GalleryTableCellFileDate">2025-08-27</td>
+        <td class="GalleryTableCellFileType">PDF</td>
+        <td class="GalleryTableCellFileSize">105 kB</td>
+      </tr>
+    </table>
+  </div>
+<?php
+  include_once '../includes/footer.php'
+?>
