cbe05fb2723973babb37d03add20a2dd425c3e4b
pownas <pownas@outlook.com>
2026-09-11 16:58:43 +0200

    Tider för uppdateringar

--- src/foreningen/organisation/styrelsen/index.php
+++ src/foreningen/organisation/styrelsen/index.php
@@ -2,7 +2,7 @@
   $header_title = "Styrelsen - Organisation - Föreningen";
   $header_description = "Information om den ideella dansföreningen Rockrullarnas styrelse";
 
-  $page_updated = "2026-08-27 08:08";
+  $page_updated = "2026-09-11 16:58";
   $page_url = "/foreningen/organisation/styrelsen";
   $page_contact_name = "Styrelsen";
   $page_contact_email = "styrelsen@rockrullarna.se";
